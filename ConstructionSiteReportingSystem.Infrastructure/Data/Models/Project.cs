7de6137f9459681b49091f474dd6fb1d92572f93
karinanikolova <lianna_kn@yahoo.com>
2024-03-02 13:21:10 +0200

    Modified Project entity class.

--- ConstructionSiteReportingSystem.Infrastructure/Data/Models/Project.cs
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Models/Project.cs
@@ -1,10 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ConstructionSiteReportingSystem.Infrastructure.Data.Utilities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ConstructionSiteReportingSystem.Infrastructure.Data.Models
 {
     [Comment("Investment project")]
     public class Project : SoftDelete
     {
+        [Key]
+        [Comment("Investment project identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [Comment("Investment project/Construction site information identifier")]
+        public int ProjectSiteInfoId { get; set; }
+
+        [ForeignKey(nameof(ProjectSiteInfoId))]
+        public ProjectSiteInfo ProjectSiteInfo { get; set; } = null!;
+
+        public ICollection<WorkByProject> WorksByProjects { get; set; } = new List<WorkByProject>();
+
     }
 }
