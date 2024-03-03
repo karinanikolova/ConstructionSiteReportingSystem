82e31f7fd3b5c59b2615879897467b6b2c0c73a3
karinanikolova <lianna_kn@yahoo.com>
2024-03-03 17:10:28 +0200

    Modified WorkByProjects entity class (added properties, comments and data annotations).

--- ConstructionSiteReportingSystem.Infrastructure/Data/Models/WorkByProject.cs
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Models/WorkByProject.cs
@@ -1,13 +1,43 @@
 ﻿using ConstructionSiteReportingSystem.Infrastructure.Data.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ConstructionSiteReportingSystem.Infrastructure.Data.Models
 {
+    [Comment("Construction and assembly work according to investment project")]
     public class WorkByProject : SoftDelete
     {
+        [Key]
+        [Comment("Construction and assembly work according to investment project identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [Comment("Construction and assembly work according to investment project additional information identifier")]
+        public int WorkInfoId { get; set; }
+
+        [ForeignKey(nameof(WorkInfoId))]
+        public WorkInfo WorkInfo { get; set; } = null!;
+
+        [Comment("Construction and assembly work according to investment project quantity")]
+        public double Quantity { get; set; }
+
+        [Required]
+        [Comment("Construction and assembly work according to investment project unit identifier")]
+        public int UnitId { get; set; }
+
+        [ForeignKey(nameof(UnitId))]
+        public Unit Unit { get; set; } = null!;
+
+        [Required]
+        [Comment("Construction and assembly work according to investment project total quantity")]
+        public double ToTalQuantity { get; set; }
+
+        [Required]
+        [Comment("Construction and assembly work investment project identifier")]
+        public int ProjectId { get; set; }
+
+        [ForeignKey(nameof(ProjectId))]
+        public Project Project { get; set; } = null!;
     }
 }
