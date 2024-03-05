34a414a07cbb8d5dd942eb468c47a601ee16086d
karinanikolova <lianna_kn@yahoo.com>
2024-03-05 14:55:05 +0200

    Made changes in the ConstructionSiteDbContext. Modified SaveChanges and SaveChangesAsync methods in order to achieve soft delete. Applied query filters.

--- ConstructionSiteReportingSystem.Infrastructure/Data/ConstructionSiteDbContext.cs
+++ ConstructionSiteReportingSystem.Infrastructure/Data/ConstructionSiteDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using ConstructionSiteReportingSystem.Infrastructure.Data.Models;
+using ConstructionSiteReportingSystem.Infrastructure.Data.Utilities.Contracts;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Task = ConstructionSiteReportingSystem.Infrastructure.Data.Models.Task;
@@ -25,10 +26,63 @@ namespace ConstructionSiteReportingSystem.Infrastructure.Data
         public DbSet<WorkByProject> WorksByProjects { get; set; }
         public DbSet<WorkInfo> WorksInfo { get; set; }
 
+        // Overriding the SaveChanges method in order to achieve soft delete
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            OnBeforeSaving();
+
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        // Overriding the SaveChangesAsync method in order to achieve soft delete.
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            OnBeforeSaving();
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            // Applying query filers to each entity in order for Entity Framework Core to ignore soft-deleted records when executing queries. The query filer will only show entities with the IsDeleted property set to 'false'.
+
+            builder.Entity<Contractor>()
+                .HasQueryFilter(c => c.IsDeleted == false);
+
+            builder.Entity<Project>()
+                .HasQueryFilter(c => c.IsDeleted == false);
+
+            builder.Entity<ProjectSiteInfo>()
+                .HasQueryFilter(c => c.IsDeleted == false);
+
+            builder.Entity<Site>()
+                .HasQueryFilter(c => c.IsDeleted == false);
+
             builder.Entity<SiteStage>()
-                .HasKey(ss => new { ss.SiteId, ss.StageId});
+                .HasQueryFilter(c => c.IsDeleted == false);
+
+            builder.Entity<Stage>()
+                .HasQueryFilter(c => c.IsDeleted == false);
+
+            builder.Entity<Task>()
+                .HasQueryFilter(c => c.IsDeleted == false);
+
+            builder.Entity<Unit>()
+                .HasQueryFilter(c => c.IsDeleted == false);
+
+            builder.Entity<Work>()
+                .HasQueryFilter(c => c.IsDeleted == false);
+
+            builder.Entity<WorkByProject>()
+                .HasQueryFilter(c => c.IsDeleted == false);
+
+            builder.Entity<WorkInfo>()
+                .HasQueryFilter(c => c.IsDeleted == false);
+
+            // Defining table keys, relations and Deletebehaviors
+
+            builder.Entity<SiteStage>()
+                .HasKey(ss => new { ss.SiteId, ss.StageId });
 
             builder.Entity<SiteStage>()
                 .HasOne(ss => ss.Site)
@@ -71,7 +125,7 @@ namespace ConstructionSiteReportingSystem.Infrastructure.Data
                 .WithMany(w => w.Works)
                 .HasForeignKey(w => w.StageId)
                 .OnDelete(DeleteBehavior.Restrict);
-            
+
             builder.Entity<Work>()
                 .HasOne(w => w.Contractor)
                 .WithMany(w => w.Works)
@@ -85,6 +139,23 @@ namespace ConstructionSiteReportingSystem.Infrastructure.Data
                 .OnDelete(DeleteBehavior.Restrict);
 
             base.OnModelCreating(builder);
+        }
+
+        /// <summary>
+        /// Implements logic of what the Change tracker should do when running into tracked objects which are marked as ‘Deleted’. The objects initially marked as ‘Deleted’ are instead marked as ‘Modified’ and their IsDeleted property – set to true. These objects' DeletedAt property is assigned a DateTimeOffset object whose date and time are set to the current Coordinated Universal Time (UTC) date and time.
+        /// </summary>
+        private void OnBeforeSaving()
+        {
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                if (entry.State == EntityState.Deleted && entry.Entity is ISoftDelete)
+                {
+                    entry.State = EntityState.Modified;
+
+                    entry.CurrentValues["IsDeleted"] = true;
+                    entry.CurrentValues["DeletedAt"] = DateTimeOffset.UtcNow;
+                }
+            }
         }
     }
 }
