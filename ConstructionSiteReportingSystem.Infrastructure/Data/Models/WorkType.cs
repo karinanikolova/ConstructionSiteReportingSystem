ea6f88c980dea13223f81cbda4ff5366c8f34bb0
karinanikolova <lianna_kn@yahoo.com>
2024-03-06 12:03:44 +0200

    Changed WorkInfo class name to WorkType.

--- ConstructionSiteReportingSystem.Infrastructure/Data/Models/WorkType.cs
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Models/WorkType.cs
@@ -1,20 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using ConstructionSiteReportingSystem.Infrastructure.Data.Utilities;
-using static ConstructionSiteReportingSystem.Infrastructure.Constants.DataConstants.WorkInfo;
+using static ConstructionSiteReportingSystem.Infrastructure.Constants.DataConstants.WorkType;
 
 namespace ConstructionSiteReportingSystem.Infrastructure.Data.Models
 {
-    [Comment("Construction and assembly work additional information")]
-    public class WorkInfo : SoftDelete
+    [Comment("Construction and assembly work type")]
+    public class WorkType : SoftDelete
     {
         [Key]
-        [Comment("Construction and assembly work additional information identifier")]
+        [Comment("Construction and assembly work type identifier")]
         public int Id { get; set; }
 
         [Required]
         [MaxLength(NameMaxLength)]
-        [Comment("Construction and assembly work name")]
+        [Comment("Construction and assembly work type name")]
         public string Name { get; set; } = string.Empty;
 
         public ICollection<Work> Works { get; set; } = new List<Work>();
