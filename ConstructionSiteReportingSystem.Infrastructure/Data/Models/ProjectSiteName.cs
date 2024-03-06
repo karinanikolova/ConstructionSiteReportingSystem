4f84b684424a535f863c32869df7b4aafbeda409
karinanikolova <lianna_kn@yahoo.com>
2024-03-06 11:45:23 +0200

    ProjectSiteName entity class added instead of ProjectSiteInfo.

---
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Models/ProjectSiteName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using ConstructionSiteReportingSystem.Infrastructure.Data.Utilities;
+using static ConstructionSiteReportingSystem.Infrastructure.Constants.DataConstants.ProjectSiteInfo;
+
+namespace ConstructionSiteReportingSystem.Infrastructure.Data.Models
+{
+    [Comment("Investment project/Construction site name")]
+    public class ProjectSiteName : SoftDelete
+    {
+        [Key]
+        [Comment("Investment project/Construction site name identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        [Comment("Investment project/Construction site name")]
+        public string Name { get; set; } = string.Empty;
+
+        public Site Site { get; set; } = null!;
+        public Project Project { get; set;} = null!;
+    }
+}
