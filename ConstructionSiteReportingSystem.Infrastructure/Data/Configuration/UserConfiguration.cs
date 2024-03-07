9f497b640e78ba907d54a0a0df936a3638f300d5
karinanikolova <lianna_kn@yahoo.com>
2024-03-07 17:00:05 +0200

    Adding UserConfiguration class for configuring IdentityUser entities.

---
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Configuration/UserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ConstructionSiteReportingSystem.Infrastructure.Data.Configuration
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<IdentityUser>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUser> builder)
+        {
+            // Seeding data for IdentityUsers
+            var data = new DataSeed();
+
+            builder.HasData(new List<IdentityUser>(data.Users));
+        }
+    }
+}
