a2b7b61b348f3342aeb6f08cb6b62886c19cff16
karinanikolova <lianna_kn@yahoo.com>
2024-03-07 17:07:59 +0200

    Adding StageConfiguration class for configuring Stage entities.

---
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Configuration/StageConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using ConstructionSiteReportingSystem.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ConstructionSiteReportingSystem.Infrastructure.Data.Configuration
+{
+    internal class StageConfiguration : IEntityTypeConfiguration<Stage>
+    {
+        public void Configure(EntityTypeBuilder<Stage> builder)
+        {
+            // Applying query filer to the entity in order for Entity Framework Core to ignore soft-deleted records when executing queries. The query filer will only show entities with the IsDeleted property set to 'false'.
+            builder.HasQueryFilter(c => c.IsDeleted == false);
+
+            // Seeding data for Stages
+            var data = new DataSeed();
+
+            builder.HasData(new List<Stage>(data.Stages));
+        }
+    }
+}
