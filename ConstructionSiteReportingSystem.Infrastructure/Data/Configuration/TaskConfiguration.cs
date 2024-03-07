0129e31443174d5c165dcdc2d398d2ea03bd90fa
karinanikolova <lianna_kn@yahoo.com>
2024-03-07 18:13:06 +0200

    Adding TaskConfiguration class for configuring Task entities.

---
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Configuration/TaskConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Task = ConstructionSiteReportingSystem.Infrastructure.Data.Models.Task;
+
+namespace ConstructionSiteReportingSystem.Infrastructure.Data.Configuration
+{
+    internal class TaskConfiguration : IEntityTypeConfiguration<Task>
+    {
+        public void Configure(EntityTypeBuilder<Task> builder)
+        {
+            // Applying query filer to the entity in order for Entity Framework Core to ignore soft-deleted records when executing queries. The query filer will only show entities with the IsDeleted property set to 'false'.
+            builder.HasQueryFilter(c => c.IsDeleted == false);
+
+            // Seeding data for Tasks
+            var data = new DataSeed();
+
+            builder.HasData(new List<Task>(data.Tasks));
+        }
+    }
+}
