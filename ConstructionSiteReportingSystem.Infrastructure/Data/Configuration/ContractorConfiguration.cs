5e750e7387c61847db1deb6d096eb122287511ed
karinanikolova <lianna_kn@yahoo.com>
2024-03-07 16:58:46 +0200

    Adding ContractorConfiguration class for configuring Constractor entities.

---
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Configuration/ContractorConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using ConstructionSiteReportingSystem.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ConstructionSiteReportingSystem.Infrastructure.Data.Configuration
+{
+    internal class ContractorConfiguration : IEntityTypeConfiguration<Contractor>
+    {
+        public void Configure(EntityTypeBuilder<Contractor> builder)
+        {
+            // Applying query filer to the entity in order for Entity Framework Core to ignore soft-deleted records when executing queries. The query filer will only show entities with the IsDeleted property set to 'false'.
+            builder.HasQueryFilter(c => c.IsDeleted == false);
+
+            // Seeding data for Contractors
+            var data = new DataSeed();
+
+            builder.HasData(new List<Contractor>(data.Contractors));
+        }
+    }
+}
