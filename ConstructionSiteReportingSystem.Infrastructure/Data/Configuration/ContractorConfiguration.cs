917189f0760ded527007f8e3ce1088315bef226d
karinanikolova <lianna_kn@yahoo.com>
2024-03-08 13:33:04 +0200

    Removed and sorted usings in ContractorConfiguration class.

--- ConstructionSiteReportingSystem.Infrastructure/Data/Configuration/ContractorConfiguration.cs
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Configuration/ContractorConfiguration.cs
@@ -1,5 +1,4 @@
 ﻿using ConstructionSiteReportingSystem.Infrastructure.Data.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
