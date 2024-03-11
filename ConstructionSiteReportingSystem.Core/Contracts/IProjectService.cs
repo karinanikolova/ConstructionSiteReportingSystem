a942299de2b2f03fc286598197be5479b5b5b662
karinanikolova <lianna_kn@yahoo.com>
2024-03-11 18:53:45 +0200

    Created IProjectService interface to hold business logic for the ProjectController.

---
+++ ConstructionSiteReportingSystem.Core/Contracts/IProjectService.cs
@@ -0,0 +1,9 @@
+﻿using ConstructionSiteReportingSystem.Core.Models.Home;
+
+namespace ConstructionSiteReportingSystem.Core.Contracts
+{
+    public interface IProjectService
+    {
+        Task<IEnumerable<ProjectIndexViewModel>> ProjectsForPreviewAsync();
+    }
+}
