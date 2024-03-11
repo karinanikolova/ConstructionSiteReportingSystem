610bdf6c9118d180a429e4ee777b4b19afdcf7fd
karinanikolova <lianna_kn@yahoo.com>
2024-03-11 18:55:40 +0200

    Modified ServiceCollectionExtension to add Repository and ProjectService to the IoC container.

--- ConstructionSIteReportingSystem/Extensions/ServiceCollectionExtension.cs
+++ ConstructionSIteReportingSystem/Extensions/ServiceCollectionExtension.cs
@@ -1,27 +1,57 @@
-﻿using ConstructionSiteReportingSystem.Infrastructure.Data;
+﻿using ConstructionSiteReportingSystem.Core.Contracts;
+using ConstructionSiteReportingSystem.Core.Services;
+using ConstructionSiteReportingSystem.Infrastructure.Data;
+using ConstructionSiteReportingSystem.Infrastructure.Data.Utilities;
+using ConstructionSiteReportingSystem.Infrastructure.Data.Utilities.Contracts;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
+    /// <summary>
+    /// Providing methods to register services in order to declutter the Program.cs file.
+    /// </summary>
     public static class ServiceCollectionExtension
     {
+        /// <summary>
+        /// Adds services holding business logic for the application to the specified IServiceCollection.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
+            // Adding ProjectService to the Inversion of Control container.
+            services.AddScoped<IProjectService, ProjectService>();
+
             return services;
         }
 
+        /// <summary>
+        /// Adds services for database to the specified IServiceCollection.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
         public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<ConstructionSiteDbContext>(options =>
                 options.UseSqlServer(connectionString));
 
+            // Adding Repository to the Inversion of Control container.
+            services.AddScoped<IRepository, Repository>();
+
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             return services;
         }
 
+        /// <summary>
+        /// Adds services for Identity to the specified IServiceCollection.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
         public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration configuration)
         {
             services
