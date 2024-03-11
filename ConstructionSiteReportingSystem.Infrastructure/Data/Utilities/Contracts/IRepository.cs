1c15795926d03f49bca9bbc0f24331bda8e3d3ba
karinanikolova <lianna_kn@yahoo.com>
2024-03-11 18:51:13 +0200

    Created IRepository interface for implementing Unit of Work pattern.

---
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Utilities/Contracts/IRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace ConstructionSiteReportingSystem.Infrastructure.Data.Utilities.Contracts
+{
+    /// <summary>
+    /// Interface to provide implementation for a single database context use across multiple services (Unit of Work pattern). It gives a layer of abstraction between the data access layer and the business logic layer and makes it easier in case of changing database providers over time.
+    /// </summary>
+    public interface IRepository
+    {
+        IQueryable<TEntity> All<TEntity>() where TEntity : class;
+
+        IQueryable<TEntity> AllReadOnly<TEntity>() where TEntity : class;
+
+        Task AddAsync<TEntity>(TEntity entity) where TEntity : class;
+
+        void Add<TEntity>(TEntity entity) where TEntity : class;
+
+        Task<int> SaveChangesAsync();
+
+        int SaveChanges();
+
+        void Delete<TEntity>(TEntity entityToDelete) where TEntity : class;
+
+        void DeleteRange<TEntity>(params TEntity[] entitiesToDelete) where TEntity : class;
+    }
+}
