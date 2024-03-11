3261d929dca274a770a4f36a91c055f4fd4d4ae8
karinanikolova <lianna_kn@yahoo.com>
2024-03-11 18:56:57 +0200

    Created ProjectController.

---
+++ ConstructionSIteReportingSystem/Controllers/ProjectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConstructionSiteReportingSystem.Controllers
+{
+    public class ProjectController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
