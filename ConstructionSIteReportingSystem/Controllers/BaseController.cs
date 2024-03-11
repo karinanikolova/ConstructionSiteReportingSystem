3fe73f6cc733a30c95910c92c700a680ebfa95d2
karinanikolova <lianna_kn@yahoo.com>
2024-03-11 14:47:17 +0200

    Created a BaseController class that inherits from the Controller class to be inherited by all the controller classes.

---
+++ ConstructionSIteReportingSystem/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConstructionSiteReportingSystem.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+
+    }
+}
