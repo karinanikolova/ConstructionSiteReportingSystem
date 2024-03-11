0cd5b08a8a55220c395e0f9a7ff00cd8672f1d49
karinanikolova <lianna_kn@yahoo.com>
2024-03-11 14:42:13 +0200

    Created a NavigationBarComponent for the navigation bar.

---
+++ ConstructionSIteReportingSystem/Components/NavigationBarComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConstructionSiteReportingSystem.Components
+{
+    public class NavigationBarComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
