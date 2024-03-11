593b46ad204e1a3d811a553ad701842e177d78fa
karinanikolova <lianna_kn@yahoo.com>
2024-03-11 18:51:56 +0200

    Created Repository class which implements IRepository.

---
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Utilities/Repository.cs
@@ -0,0 +1,63 @@
+﻿using ConstructionSiteReportingSystem.Infrastructure.Data.Utilities.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConstructionSiteReportingSystem.Infrastructure.Data.Utilities
+{
+    /// <summary>
+    /// Implements the IRepository interface and provides Unit of Work pattern functionality.
+    /// </summary>
+    public class Repository : IRepository
+    {
+        private readonly DbContext _context;
+
+        public Repository(ConstructionSiteDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add<TEntity>(TEntity entity) where TEntity : class
+        {
+            DbSet<TEntity>().Add(entity);
+        }
+
+        public async Task AddAsync<TEntity>(TEntity entity) where TEntity : class
+        {
+            await DbSet<TEntity>().AddAsync(entity);
+        }
+
+        public IQueryable<TEntity> All<TEntity>() where TEntity : class
+        {
+            return DbSet<TEntity>();
+        }
+
+        public IQueryable<TEntity> AllReadOnly<TEntity>() where TEntity : class
+        {
+            return DbSet<TEntity>().AsNoTracking();
+        }
+
+        public void Delete<TEntity>(TEntity entityToDelete) where TEntity : class
+        {
+            DbSet<TEntity>().Remove(entityToDelete);
+        }
+
+        public void DeleteRange<TEntity>(params TEntity[] entitiesToDelete) where TEntity : class
+        {
+            DbSet<TEntity>().RemoveRange(entitiesToDelete);
+        }
+
+        public int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        private DbSet<TEntity> DbSet<TEntity>() where TEntity : class
+        {
+            return _context.Set<TEntity>();
+        }
+    }
+}
