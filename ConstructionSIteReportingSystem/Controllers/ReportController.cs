aed7cc7ce241abf8f0ef53d0d432d4e19e11e329
karinanikolova <lianna_kn@yahoo.com>
2024-03-11 18:57:13 +0200

    Created ReportController.

---
+++ ConstructionSIteReportingSystem/Controllers/ReportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConstructionSiteReportingSystem.Controllers
+{
+    public class ReportController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
