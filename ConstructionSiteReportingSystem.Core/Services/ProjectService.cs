6cffdf699ed23e3157013a4dfcd1e6f018aac444
karinanikolova <lianna_kn@yahoo.com>
2024-03-11 18:54:12 +0200

    Created ProjectService class to implement IProjectService.

---
+++ ConstructionSiteReportingSystem.Core/Services/ProjectService.cs
@@ -0,0 +1,13 @@
+﻿using ConstructionSiteReportingSystem.Core.Contracts;
+using ConstructionSiteReportingSystem.Core.Models.Home;
+
+namespace ConstructionSiteReportingSystem.Core.Services
+{
+    public class ProjectService : IProjectService
+    {
+        public Task<IEnumerable<ProjectIndexViewModel>> ProjectsForPreviewAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
