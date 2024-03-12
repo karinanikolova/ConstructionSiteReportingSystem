32aad74764da1d515ea54c4ec3d3171a89a6b08c
karinanikolova <lianna_kn@yahoo.com>
2024-03-12 19:24:13 +0200

    Created IHomeService interface.

---
+++ ConstructionSiteReportingSystem.Core/Contracts/IHomeService.cs
@@ -0,0 +1,9 @@
+﻿using ConstructionSiteReportingSystem.Core.Models.Home;
+
+namespace ConstructionSiteReportingSystem.Core.Contracts
+{
+	public interface IHomeService
+	{
+		Task<IEnumerable<IndexViewModel>> ProjectsForPreviewAsync();
+	}
+}
