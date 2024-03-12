23c080afc85bb08763e54fb3467acb63e89bd91e
karinanikolova <lianna_kn@yahoo.com>
2024-03-12 19:30:29 +0200

    Modified IndexViewModel.

--- ConstructionSiteReportingSystem.Core/Models/Home/IndexViewModel.cs
+++ ConstructionSiteReportingSystem.Core/Models/Home/IndexViewModel.cs
@@ -2,5 +2,10 @@
 {
     public class IndexViewModel
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string ImageUrl { get; set; } = string.Empty;
     }
 }
