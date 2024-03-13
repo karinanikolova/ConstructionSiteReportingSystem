1e404d08c79a3c4dd22def46710093de7a0466df
karinanikolova <lianna_kn@yahoo.com>
2024-03-13 15:35:02 +0200

    Modified ProjectController.

--- ConstructionSIteReportingSystem/Controllers/ProjectController.cs
+++ ConstructionSIteReportingSystem/Controllers/ProjectController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ConstructionSiteReportingSystem.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ConstructionSiteReportingSystem.Controllers
 {
     public class ProjectController : BaseController
     {
-        public IActionResult Index()
+		private readonly ILogger<ProjectController> _logger;
+		private readonly IProjectService _projectService;
+
+		public ProjectController(ILogger<ProjectController> logger, IProjectService projectService)
+		{
+			_logger = logger;
+			_projectService = projectService;
+		}
+
+		[HttpGet]
+        public async Task<IActionResult> All()
         {
-            return View();
+			var models = await _projectService.AllProjectsAsync();
+
+            return View(models);
         }
     }
 }
