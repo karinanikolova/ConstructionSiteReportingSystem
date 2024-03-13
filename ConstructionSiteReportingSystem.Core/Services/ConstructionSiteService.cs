8b4609ddd993b1bed65194bfc1ec73ba07cbfd30
karinanikolova <lianna_kn@yahoo.com>
2024-03-13 15:27:52 +0200

    Added ConstructionSiteService class.

---
+++ ConstructionSiteReportingSystem.Core/Services/ConstructionSiteService.cs
@@ -0,0 +1,42 @@
+﻿using ConstructionSiteReportingSystem.Core.Contracts;
+using ConstructionSiteReportingSystem.Core.Models.Project;
+using ConstructionSiteReportingSystem.Core.Models.Site;
+using ConstructionSiteReportingSystem.Infrastructure.Data.Models;
+using ConstructionSiteReportingSystem.Infrastructure.Data.Utilities.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionSiteReportingSystem.Core.Services
+{
+	public class ConstructionSiteService : IConstructionSiteService
+	{
+		private readonly IRepository _repository;
+
+		public ConstructionSiteService(IRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<SiteViewModel> GetSiteAsync(int projectSiteNameId)
+		{
+			return await _repository.AllReadOnly<Site>()
+				.Where(s => s.ProjectSiteNameId == projectSiteNameId)
+				.Select(s => new SiteViewModel()
+				{
+					FinishDate = s.FinishDate,
+					Stages = s.SitesStages
+					.Where(ss => ss.SiteId == s.Id)
+					.Select (stage => new StageViewModel()
+					{
+						Name = stage.Stage.Name
+					})
+					.ToList()
+				})
+				.FirstOrDefaultAsync();
+		}
+	}
+}
