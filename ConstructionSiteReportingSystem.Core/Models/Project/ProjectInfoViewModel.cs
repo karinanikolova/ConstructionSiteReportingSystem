5300cac45362b336cd1360143245d12abcda6da3
karinanikolova <lianna_kn@yahoo.com>
2024-03-13 15:32:50 +0200

    Added ProjectViewModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Project/ProjectInfoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ConstructionSiteReportingSystem.Core.Models.Project
+{
+    public class ProjectInfoViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
