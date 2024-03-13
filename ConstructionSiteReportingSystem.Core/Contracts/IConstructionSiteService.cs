e95affa5301c92c8a713d8949ba64c14576d22bd
karinanikolova <lianna_kn@yahoo.com>
2024-03-13 15:25:27 +0200

    Added IConstructionSiteService interface.

---
+++ ConstructionSiteReportingSystem.Core/Contracts/IConstructionSiteService.cs
@@ -0,0 +1,9 @@
+﻿using ConstructionSiteReportingSystem.Core.Models.Site;
+
+namespace ConstructionSiteReportingSystem.Core.Contracts
+{
+	public interface IConstructionSiteService
+	{
+		Task<SiteViewModel> GetSiteAsync(int projectSiteNameId);
+	}
+}
