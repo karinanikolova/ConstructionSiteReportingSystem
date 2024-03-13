16ad54bf51ef82638d36abfb3f686d6ca85cb261
karinanikolova <lianna_kn@yahoo.com>
2024-03-13 15:35:21 +0200

    Created ConstructionSiteController.

---
+++ ConstructionSIteReportingSystem/Controllers/ConstructionSiteController.cs
@@ -0,0 +1,30 @@
+﻿using ConstructionSiteReportingSystem.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConstructionSiteReportingSystem.Controllers
+{
+	public class ConstructionSiteController : BaseController
+	{
+		private readonly ILogger<ConstructionSiteController> _logger;
+		private readonly IConstructionSiteService _constructionSiteService;
+
+		public ConstructionSiteController(ILogger<ConstructionSiteController> logger, IConstructionSiteService constructionSiteService)
+		{
+			_logger = logger;
+			_constructionSiteService = constructionSiteService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> ConstructionSite(int id)
+		{
+			var site = await _constructionSiteService.GetSiteAsync(id);
+
+			if (site == null)
+			{
+				return BadRequest();
+			}
+
+			return View(site);
+		}
+	}
+}
