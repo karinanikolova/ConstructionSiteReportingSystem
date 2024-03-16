5a00e8f01225dcafe222a02628d843573cd2c8d6
karinanikolova <lianna_kn@yahoo.com>
2024-03-16 12:07:44 +0200

    Created methods in IConstructionSiteService.

--- ConstructionSiteReportingSystem.Core/Contracts/IConstructionSiteService.cs
+++ ConstructionSiteReportingSystem.Core/Contracts/IConstructionSiteService.cs
@@ -1,9 +1,10 @@
 ﻿using ConstructionSiteReportingSystem.Core.Models.Site;
+using ConstructionSiteReportingSystem.Infrastructure.Enums;
 
 namespace ConstructionSiteReportingSystem.Core.Contracts
 {
 	public interface IConstructionSiteService
 	{
-		Task<SiteViewModel> GetSiteAsync(int projectSiteNameId);
+		Task<SiteQueryServiceModel> GetSiteAsync(int projectSiteNameId, string? stage = null, string? searchDate = null, DateSorting dateSorting = DateSorting.Newest, int currentPage = 1, int worksPerPage = 1);
 	}
 }
