298d64e57bd584330edbbe7128cb9246ffba6b86
karinanikolova <lianna_kn@yahoo.com>
2024-03-16 12:22:33 +0200

    Modified SiteViewModel.

--- ConstructionSiteReportingSystem.Core/Models/Site/SiteViewModel.cs
+++ ConstructionSiteReportingSystem.Core/Models/Site/SiteViewModel.cs
@@ -2,6 +2,8 @@
 {
     public class SiteViewModel
     {
+        public string Name { get; set; } = string.Empty;
+
         public DateTime FinishDate { get; set; }
 
         public IEnumerable<StageViewModel> Stages { get; set; } = new List<StageViewModel>();
