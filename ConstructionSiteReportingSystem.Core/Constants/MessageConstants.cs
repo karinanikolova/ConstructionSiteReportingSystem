2190f38410ead41fd541f3b2016bd21dc97b34a7
karinanikolova <lianna_kn@yahoo.com>
2024-03-16 12:23:11 +0200

    Added MessageConstants.

---
+++ ConstructionSiteReportingSystem.Core/Constants/MessageConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionSiteReportingSystem.Core.Constants
+{
+    public static class MessageConstants
+    {
+        public const string DateMessage = "Field {0} must contain exactly {0} characters";
+    }
+}
