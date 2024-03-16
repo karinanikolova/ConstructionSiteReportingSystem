7ef8e14a82a1beb6e08c849e76d29c2a2c60c099
karinanikolova <lianna_kn@yahoo.com>
2024-03-16 12:21:36 +0200

    Added SiteQueryModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Site/SiteQueryModel.cs
@@ -0,0 +1,28 @@
+﻿using ConstructionSiteReportingSystem.Core.Constants;
+using ConstructionSiteReportingSystem.Infrastructure.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConstructionSiteReportingSystem.Core.Models.Site
+{
+    public class SiteQueryModel
+    {
+        public const int WorksPerPage = 5;
+
+        public string Stage { get; init; } = null!;
+
+        [Display(Name = "Search by date")]
+        public string? SearchDate { get; init; }
+
+        public DateSorting Sorting { get; init; }
+
+        public int CurrentPage { get; init; } = 1;
+
+        public int TotalWorksCount { get; set; }
+
+        public string SiteName { get; set; } = string.Empty;
+
+        public DateTime ConstructionFinishDate { get; set; }
+
+        public IEnumerable<StageViewModel> StagesWithWorks { get; set; } = new List<StageViewModel>();
+    }
+}
