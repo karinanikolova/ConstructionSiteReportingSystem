c2ef9a8429d61f17a6fa42ef6bb05e838f6ea8e1
karinanikolova <lianna_kn@yahoo.com>
2024-03-16 12:22:18 +0200

    Added WorkViewModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Site/WorkViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace ConstructionSiteReportingSystem.Core.Models.Site
+{
+    public class WorkViewModel
+    {
+        public int Id { get; set; }
+
+        public string WorkName { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public DateTime CarryOutDate { get; set; }
+
+        public string Contractor { get; set; } = string.Empty;
+
+        public double Quantity { get; set; }
+
+        public string Unit { get; set; } = string.Empty;
+
+        public decimal CostPerUnit { get; set; }
+
+        public decimal TotalCost { get; set; }
+
+        public string CreatorId { get; set; } = string.Empty;
+    }
+}
