c653f57896930e953d2ee64909b14edf3fd22f25
karinanikolova <lianna_kn@yahoo.com>
2024-03-16 12:21:57 +0200

    Added SiteQueryServiceModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Site/SiteQueryServiceModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ConstructionSiteReportingSystem.Core.Models.Site
+{
+    public class SiteQueryServiceModel
+    {
+        public string SiteName { get; set; } = string.Empty;
+
+        public DateTime ConstructionFinishDate { get; set; }
+
+        public int TotalWorksCount { get; set; }
+
+        public ICollection<StageViewModel> StagesWithWorks { get; set; } = new List<StageViewModel>();
+    }
+}
