034a01e9e72c8bd4b39c41e600e34a70f367a009
karinanikolova <lianna_kn@yahoo.com>
2024-03-18 15:50:18 +0200

    Modified IConstructionSiteService.

--- ConstructionSiteReportingSystem.Core/Contracts/IConstructionSiteService.cs
+++ ConstructionSiteReportingSystem.Core/Contracts/IConstructionSiteService.cs
@@ -6,5 +6,7 @@ namespace ConstructionSiteReportingSystem.Core.Contracts
 	public interface IConstructionSiteService
 	{
 		Task<SiteQueryServiceModel> GetSiteAsync(int projectSiteNameId, string? stage = null, string? searchDate = null, DateSorting dateSorting = DateSorting.Newest, int currentPage = 1, int worksPerPage = 1);
+
+		Task<IEnumerable<string>> GetAllStagesNamesAsync();
 	}
 }
