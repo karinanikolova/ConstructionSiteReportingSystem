c3a33e44601492326d2677cecd0fd9f88ff1b8e3
karinanikolova <lianna_kn@yahoo.com>
2024-03-18 15:50:40 +0200

    ModifiedConstructionSiteService.

--- ConstructionSiteReportingSystem.Core/Services/ConstructionSiteService.cs
+++ ConstructionSiteReportingSystem.Core/Services/ConstructionSiteService.cs
@@ -6,156 +6,163 @@ using ConstructionSiteReportingSystem.Infrastructure.Enums;
 using Microsoft.EntityFrameworkCore;
 using System.Globalization;
 using static ConstructionSiteReportingSystem.Core.Constants.ValidationConstants;
-using static ConstructionSiteReportingSystem.Infrastructure.Constants.DataConstants;
-using Work = ConstructionSiteReportingSystem.Infrastructure.Data.Models.Work;
+using Stage = ConstructionSiteReportingSystem.Infrastructure.Data.Models.Stage;
 
 namespace ConstructionSiteReportingSystem.Core.Services
 {
-    public class ConstructionSiteService : IConstructionSiteService
-    {
-        private readonly IRepository _repository;
+	public class ConstructionSiteService : IConstructionSiteService
+	{
+		private readonly IRepository _repository;
 
-        public ConstructionSiteService(IRepository repository)
-        {
-            _repository = repository;
-        }
+		public ConstructionSiteService(IRepository repository)
+		{
+			_repository = repository;
+		}
 
-        public async Task<SiteQueryServiceModel> GetSiteAsync(int projectSiteNameId, string? stage = null, string? searchDate = null, DateSorting dateSorting = DateSorting.Newest, int currentPage = 1, int worksPerPage = 1)
-        {
-            var site = await _repository.AllReadOnly<Site>()
-                .Where(s => s.ProjectSiteNameId == projectSiteNameId)
-                .Select(s => new SiteQueryServiceModel()
-                {
-                    SiteName = s.ProjectSiteName.Name,
-                    ConstructionFinishDate = s.FinishDate
-                })
-                .FirstOrDefaultAsync();
+		public async Task<SiteQueryServiceModel> GetSiteAsync(int projectSiteNameId, string? stage = null, string? searchDate = null, DateSorting dateSorting = DateSorting.Newest, int currentPage = 1, int worksPerPage = 1)
+		{
+			var site = await _repository.AllReadOnly<Site>()
+				.Where(s => s.ProjectSiteNameId == projectSiteNameId)
+				.Select(s => new SiteQueryServiceModel()
+				{
+					SiteName = s.ProjectSiteName.Name,
+					ConstructionFinishDate = s.FinishDate
+				})
+				.FirstOrDefaultAsync();
 
-            if (site != null)
-            {
-                //Fix stages input -> should give all stages to the stage list
-                var siteStagesWithWorks = _repository.AllReadOnly<SiteStage>()
-                .Where(ss => ss.Site.ProjectSiteNameId == projectSiteNameId);
+			if (site != null)
+			{
+				var siteStagesWithWorks = _repository.AllReadOnly<SiteStage>()
+				.Where(ss => ss.Site.ProjectSiteNameId == projectSiteNameId);
 
-                if (!string.IsNullOrWhiteSpace(stage))
-                {
-                    siteStagesWithWorks = _repository.AllReadOnly<SiteStage>()
-                .Where(ss => ss.Site.ProjectSiteNameId == projectSiteNameId && ss.Stage.Name == stage);
-                }
+				var allStages = await GetAllStagesNamesAsync();
+				
 
-                DateTime date;
-                bool isDateValid = DateTime.TryParseExact(searchDate, DateTimeFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out date);
+				if (!string.IsNullOrWhiteSpace(stage) && allStages.Any(s => s == stage))
+				{
+					siteStagesWithWorks = _repository.AllReadOnly<SiteStage>()
+				.Where(ss => ss.Site.ProjectSiteNameId == projectSiteNameId && ss.Stage.Name == stage);
+				}
 
-                if (isDateValid)
-                {
-                    //Fix filtering by specific date
-                    siteStagesWithWorks = siteStagesWithWorks
-                        .Where(sw => sw.Stage.Works.Any(w => w.CarryOutDate == date));
+				DateTime date;
+				bool isDateValid = DateTime.TryParseExact(searchDate, DateTimeFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out date);
 
-                    if (stage == null)
-                    {
-                        var stageModel = siteStagesWithWorks
-                            .Select(sw => new StageViewModel()
-                            {
-                                Id = sw.Stage.Id,
-                                Name = sw.Stage.Name,
-                                Works = sw.Stage.Works
-                                .Where(w => w.CarryOutDate == date)
-                                .Select(w => new WorkViewModel()
-                                {
-                                    Id = w.Id,
-                                    WorkName = w.WorkType.Name,
-                                    Description = w.Description,
-                                    CarryOutDate = w.CarryOutDate,
-                                    Contractor = w.Contractor.Name,
-                                    Quantity = w.Quantity,
-                                    Unit = w.Unit.Type,
-                                    CostPerUnit = w.CostPerUnit,
-                                    TotalCost = w.TotalCost,
-                                    CreatorId = w.CreatorId
-                                })
-                                .ToList()
-                            })
-                            .First();
+				if (isDateValid)
+				{
+					var stageModel = siteStagesWithWorks
+							.Select(sw => new StageViewModel()
+							{
+								Id = sw.Stage.Id,
+								Name = sw.Stage.Name,
+								Works = sw.Stage.Works
+								.Where(w => w.CarryOutDate.Year == date.Year && w.CarryOutDate.Month == date.Month && w.CarryOutDate.Day == date.Day)
+								.Select(w => new WorkViewModel()
+								{
+									Id = w.Id,
+									WorkName = w.WorkType.Name,
+									Description = w.Description,
+									CarryOutDate = w.CarryOutDate,
+									Contractor = w.Contractor.Name,
+									Quantity = w.Quantity,
+									Unit = w.Unit.Type,
+									CostPerUnit = w.CostPerUnit,
+									TotalCost = w.TotalCost,
+									CreatorId = w.CreatorId
+								})
+								.ToList()
+							})
+							.First();
 
-                        site.StagesWithWorks.Add(stageModel);
-                    }
-                    else
-                    {
-                        var stageModels = siteStagesWithWorks
-                            .Select(sw => new StageViewModel()
-                            {
-                                Id = sw.Stage.Id,
-                                Name = sw.Stage.Name,
-                                Works = sw.Stage.Works
-                                .Select(w => new WorkViewModel()
-                                {
-                                    Id = w.Id,
-                                    WorkName = w.WorkType.Name,
-                                    Description = w.Description,
-                                    CarryOutDate = w.CarryOutDate,
-                                    Contractor = w.Contractor.Name,
-                                    Quantity = w.Quantity,
-                                    Unit = w.Unit.Type,
-                                    CostPerUnit = w.CostPerUnit,
-                                    TotalCost = w.TotalCost,
-                                    CreatorId = w.CreatorId
-                                })
-                                .ToList()
-                            })
-                            .ToList();
+					site.StagesWithWorks.Add(stageModel);
+				}
+				else
+				{
+					var stageModels = siteStagesWithWorks
+							 .Select(sw => new StageViewModel()
+							 {
+								 Id = sw.Stage.Id,
+								 Name = sw.Stage.Name,
+								 Works = sw.Stage.Works
+								 .Select(w => new WorkViewModel()
+								 {
+									 Id = w.Id,
+									 WorkName = w.WorkType.Name,
+									 Description = w.Description,
+									 CarryOutDate = w.CarryOutDate,
+									 Contractor = w.Contractor.Name,
+									 Quantity = w.Quantity,
+									 Unit = w.Unit.Type,
+									 CostPerUnit = w.CostPerUnit,
+									 TotalCost = w.TotalCost,
+									 CreatorId = w.CreatorId
+								 })
+								 .ToList()
+							 })
+							 .ToList();
 
-                        site.StagesWithWorks = stageModels;
-                    }
-                }
-                else
-                {
-                    var stagesIds = siteStagesWithWorks.Select(s => s.StageId);
+					var works = new List<WorkViewModel>();
 
-                    var stageModels = siteStagesWithWorks
-                             .Select(sw => new StageViewModel()
-                             {
-                                 Id = sw.Stage.Id,
-                                 Name = sw.Stage.Name,
-                                 Works = sw.Stage.Works
-                                 .Select(w => new WorkViewModel()
-                                 {
-                                     Id = w.Id,
-                                     WorkName = w.WorkType.Name,
-                                     Description = w.Description,
-                                     CarryOutDate = w.CarryOutDate,
-                                     Contractor = w.Contractor.Name,
-                                     Quantity = w.Quantity,
-                                     Unit = w.Unit.Type,
-                                     CostPerUnit = w.CostPerUnit,
-                                     TotalCost = w.TotalCost,
-                                     CreatorId = w.CreatorId
-                                 })
-                                 .ToList()
-                             })
-                             .ToList();
+					if (stageModels.Count() > 1)
+					{
+						foreach (var workModels in stageModels.Select(ss => ss.Works))
+						{
+							foreach (var work in workModels)
+							{
+								works.Add(work);
+							}
+						}
 
-                    foreach (var stageId in stagesIds)
-                    {
-                        if (dateSorting == DateSorting.Newest)
-                        {
-                            stageModels.Where(s => s.Id == stageId)
-                                .Select(s => s.Works.OrderBy(w => w.CarryOutDate).ToList());
-                        }
-                        else
-                        {
-                            stageModels.Where(s => s.Id == stageId)
-                                .Select(s => s.Works.OrderByDescending(w => w.CarryOutDate).ToList());
-                        }
-                    }
+						works = dateSorting switch
+						{
+							DateSorting.Newest => works.OrderBy(w => w.CarryOutDate).ToList(),
+							DateSorting.Oldest => works.OrderByDescending(w => w.CarryOutDate).ToList(),
+							_ => works.OrderBy(w => w.CarryOutDate).ToList()
+						};
 
-                    site.StagesWithWorks = stageModels;
-                }
+						var allWorksModel = new List<StageViewModel>();
 
-                site.TotalWorksCount = site.StagesWithWorks.Select(sw => sw.Works.Count).Sum();
-            }
+						allWorksModel.Add(new StageViewModel()
+						{
+							Works = works
+						});
 
-            return site;
-        }
-    }
+						site.StagesWithWorks = allWorksModel;
+					}
+					else
+					{
+						foreach (var workModels in stageModels.Select(s => s.Works))
+						{
+							foreach (var work in workModels)
+							{
+								works.Add(work);
+							}
+						}
+
+						works = dateSorting switch
+						{
+							DateSorting.Newest => works.OrderBy(w => w.CarryOutDate).ToList(),
+							DateSorting.Oldest => works.OrderByDescending(w => w.CarryOutDate).ToList(),
+							_ => works.OrderBy(w => w.CarryOutDate).ToList()
+						};
+
+						stageModels.First().Works = works;
+
+						site.StagesWithWorks = stageModels;
+					}
+				}
+
+				site.TotalWorksCount = site.StagesWithWorks.Select(sw => sw.Works.Count).Sum();
+			}
+
+			return site;
+		}
+
+		public async Task<IEnumerable<string>> GetAllStagesNamesAsync()
+		{
+			return await _repository.AllReadOnly<Stage>()
+				.Select(s => s.Name)
+				.Distinct()
+				.ToListAsync();
+		}
+	}
 }
