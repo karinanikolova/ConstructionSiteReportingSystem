8fcf6ce431eb75a6d2456c975fe04b9c73cbf767
karinanikolova <lianna_kn@yahoo.com>
2024-03-19 18:10:54 +0200

    Added SiteInfoViewModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Site/SiteInfoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ConstructionSiteReportingSystem.Core.Models.Site
+{
+    public class SiteInfoViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string FinishDate { get; set; } = string.Empty;
+    }
+}
