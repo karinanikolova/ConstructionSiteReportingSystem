b93828d6cf07fe0dc80a228f3ea11541203038f8
karinanikolova <lianna_kn@yahoo.com>
2024-03-19 15:13:41 +0200

    Added properties from deleted entities to Site entity.

--- ConstructionSiteReportingSystem.Infrastructure/Data/Models/Site.cs
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Models/Site.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ConstructionSiteReportingSystem.Infrastructure.Data.Utilities;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using ConstructionSiteReportingSystem.Infrastructure.Data.Utilities;
+using static ConstructionSiteReportingSystem.Infrastructure.Constants.DataConstants.Site;
 
 namespace ConstructionSiteReportingSystem.Infrastructure.Data.Models
 {
-    [Comment("Construction site")]
+	[Comment("Construction site")]
     public class Site : SoftDelete
     {
         [Key]
@@ -13,16 +13,18 @@ namespace ConstructionSiteReportingSystem.Infrastructure.Data.Models
         public int Id { get; set; }
 
         [Required]
-        [Comment("Investment project/Construction site name identifier")]
-        public int ProjectSiteNameId { get; set; }
-
-        [ForeignKey(nameof(ProjectSiteNameId))]
-        public ProjectSiteName ProjectSiteName { get; set; } = null!;
+		[MaxLength(NameMaxLength)]
+		[Comment("Construction site name")]
+        public string Name { get; set; } = string.Empty;
 
         [Required]
         [Comment("Construction site finish date")]
         public DateTime FinishDate { get; set; }
 
-        public ICollection<SiteStage> SitesStages { get; set; } = new List<SiteStage>();
+		[Required]
+		[Comment("Construction site image URL")]
+		public string ImageUrl { get; set; } = string.Empty;
+
+		public ICollection<SiteStage> SitesStages { get; set; } = new List<SiteStage>();
     }
 }
