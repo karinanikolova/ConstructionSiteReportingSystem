5cd3de789b9a6fc9298471acb1fba08285733cfc
karinanikolova <lianna_kn@yahoo.com>
2024-03-19 18:09:53 +0200

    Modified methods in IConstructionSiteController.

--- ConstructionSiteReportingSystem.Core/Contracts/IConstructionSiteService.cs
+++ ConstructionSiteReportingSystem.Core/Contracts/IConstructionSiteService.cs
@@ -8,5 +8,7 @@ namespace ConstructionSiteReportingSystem.Core.Contracts
 		Task<SiteQueryServiceModel> GetSiteAsync(int projectSiteNameId, string? stage = null, string? searchDate = null, DateSorting dateSorting = DateSorting.Newest, int currentPage = 1, int worksPerPage = 1);
 
 		Task<IEnumerable<string>> GetAllStagesNamesAsync();
+
+		Task<IEnumerable<SiteInfoViewModel>> AllSitesAsync();
 	}
 }
