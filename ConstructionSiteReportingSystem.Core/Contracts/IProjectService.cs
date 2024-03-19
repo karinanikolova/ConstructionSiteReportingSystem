f0918df86af8a6282e2aacc0b142cf7c038d0c6a
karinanikolova <lianna_kn@yahoo.com>
2024-03-19 18:05:52 +0200

    Deleted IProjectService.

--- ConstructionSiteReportingSystem.Core/Contracts/IProjectService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ConstructionSiteReportingSystem.Core.Models.Project;
-
-namespace ConstructionSiteReportingSystem.Core.Contracts
-{
-    public interface IProjectService
-    {
-        Task<IEnumerable<ProjectInfoViewModel>> AllProjectsAsync();
-    }
-}
