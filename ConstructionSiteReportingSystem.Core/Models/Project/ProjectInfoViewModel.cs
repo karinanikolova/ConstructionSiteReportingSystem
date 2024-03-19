176492e87b8cede7230616ab798f574c740ec2ec
karinanikolova <lianna_kn@yahoo.com>
2024-03-19 18:04:24 +0200

    Deleted ProjectInfoViewModel.

--- ConstructionSiteReportingSystem.Core/Models/Project/ProjectInfoViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConstructionSiteReportingSystem.Core.Models.Project
-{
-    public class ProjectInfoViewModel
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; } = string.Empty;
-    }
-}
