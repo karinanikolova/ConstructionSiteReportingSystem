50b1557ccd97cf7c7a62793339c73c07b3d1954f
karinanikolova <lianna_kn@yahoo.com>
2024-03-27 15:53:00 +0200

    Created new migration for database tables creation.

---
+++ ConstructionSiteReportingSystem.Infrastructure/Migrations/20240327101709_InitialTablesCreation.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConstructionSiteReportingSystem.Infrastructure.Migrations
+{
+    public partial class InitialTablesCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contractors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Construction contractor identifier")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false, comment: "Construction contractor name"),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contractors", x => x.Id);
+                },
+                comment: "Construction contractor");
+
+            migrationBuilder.CreateTable(
+                name: "Sites",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Construction site identifier")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false, comment: "Construction site name"),
+                    FinishDate = table.Column<DateTime>(type: "datetime2", nullable: false, comment: "Construction site finish date"),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false, comment: "Construction site image URL"),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sites", x => x.Id);
+                },
+                comment: "Construction site");
+
+            migrationBuilder.CreateTable(
+                name: "Stages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Construction stage identifier")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false, comment: "Construction stage name"),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stages", x => x.Id);
+                },
+                comment: "Construction stage");
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Task identifier")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false, comment: "Task title"),
+                    Description = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false, comment: "Task description"),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false, comment: "Task creation date"),
+                    CreatorId = table.Column<string>(type: "nvarchar(450)", nullable: false, comment: "Task creator identifier"),
+                    Status = table.Column<int>(type: "int", nullable: false, comment: "Task's current status"),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tasks_AspNetUsers_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                },
+                comment: "User tasks");
+
+            migrationBuilder.CreateTable(
+                name: "Units",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Measurement unit identifier")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false, comment: "Measurement unit type"),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Units", x => x.Id);
+                },
+                comment: "Measurement unit");
+
+            migrationBuilder.CreateTable(
+                name: "WorksTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Construction and assembly work type identifier")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false, comment: "Construction and assembly work type name"),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorksTypes", x => x.Id);
+                },
+                comment: "Construction and assembly work type");
+
+            migrationBuilder.CreateTable(
+                name: "Works",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Construction and assembly work identifier")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WorkTypeId = table.Column<int>(type: "int", nullable: false, comment: "Construction and assembly work type identifier"),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true, comment: "Construction and assembly work description"),
+                    CarryOutDate = table.Column<DateTime>(type: "datetime2", nullable: false, comment: "Construction and assembly work carry out date and time"),
+                    StageId = table.Column<int>(type: "int", nullable: false, comment: "Construction and assembly work stage identifier"),
+                    ContractorId = table.Column<int>(type: "int", nullable: false, comment: "Construction and assembly work contractor identifier"),
+                    Quantity = table.Column<double>(type: "float", nullable: false, comment: "Construction and assembly work quantity"),
+                    UnitId = table.Column<int>(type: "int", nullable: false, comment: "Construction and assembly work measurement unit identifier"),
+                    CostPerUnit = table.Column<decimal>(type: "decimal(18,2)", nullable: false, comment: "Construction and assembly work cost per measurement unit"),
+                    TotalCost = table.Column<decimal>(type: "decimal(36,2)", nullable: false, comment: "Construction and assembly work total cost"),
+                    CreatorId = table.Column<string>(type: "nvarchar(450)", nullable: false, comment: "Construction and assembly work creator identifier"),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Works", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Works_AspNetUsers_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Works_Contractors_ContractorId",
+                        column: x => x.ContractorId,
+                        principalTable: "Contractors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Works_Stages_StageId",
+                        column: x => x.StageId,
+                        principalTable: "Stages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Works_Units_UnitId",
+                        column: x => x.UnitId,
+                        principalTable: "Units",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Works_WorksTypes_WorkTypeId",
+                        column: x => x.WorkTypeId,
+                        principalTable: "WorksTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                },
+                comment: "Construction and assembly work");
+
+            migrationBuilder.CreateTable(
+                name: "SitesWorks",
+                columns: table => new
+                {
+                    SiteId = table.Column<int>(type: "int", nullable: false, comment: "Construction site identifier"),
+                    WorkId = table.Column<int>(type: "int", nullable: false, comment: "Construction and assembly work identifier"),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SitesWorks", x => new { x.SiteId, x.WorkId });
+                    table.ForeignKey(
+                        name: "FK_SitesWorks_Sites_SiteId",
+                        column: x => x.SiteId,
+                        principalTable: "Sites",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_SitesWorks_Works_WorkId",
+                        column: x => x.WorkId,
+                        principalTable: "Works",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                },
+                comment: "Construction site and construction/assembly work mapping table");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SitesWorks_WorkId",
+                table: "SitesWorks",
+                column: "WorkId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_CreatorId",
+                table: "Tasks",
+                column: "CreatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Works_ContractorId",
+                table: "Works",
+                column: "ContractorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Works_CreatorId",
+                table: "Works",
+                column: "CreatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Works_StageId",
+                table: "Works",
+                column: "StageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Works_UnitId",
+                table: "Works",
+                column: "UnitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Works_WorkTypeId",
+                table: "Works",
+                column: "WorkTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SitesWorks");
+
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Sites");
+
+            migrationBuilder.DropTable(
+                name: "Works");
+
+            migrationBuilder.DropTable(
+                name: "Contractors");
+
+            migrationBuilder.DropTable(
+                name: "Stages");
+
+            migrationBuilder.DropTable(
+                name: "Units");
+
+            migrationBuilder.DropTable(
+                name: "WorksTypes");
+        }
+    }
+}
