0886657fd7410b362cdc02112a8d974c97af18d7
karinanikolova <lianna_kn@yahoo.com>
2024-03-27 18:47:22 +0200

    Created UnitServiceModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Work/UnitServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ConstructionSiteReportingSystem.Core.Models.Work
+{
+	public class UnitServiceModel
+	{
+		public int Id { get; set; }
+
+		public string Type { get; set; } = string.Empty;
+	}
+}
