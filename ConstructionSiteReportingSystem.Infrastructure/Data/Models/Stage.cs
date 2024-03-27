ed2316d929d82cfcc7c2d1e29c5a7b5813861f90
karinanikolova <lianna_kn@yahoo.com>
2024-03-27 15:49:42 +0200

    Modified Stage entity with regards to previously deleted SiteStage entity.

--- ConstructionSiteReportingSystem.Infrastructure/Data/Models/Stage.cs
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Models/Stage.cs
@@ -17,8 +17,6 @@ namespace ConstructionSiteReportingSystem.Infrastructure.Data.Models
         [Comment("Construction stage name")]
         public string Name { get; set; } = string.Empty;
 
-        public ICollection<SiteStage> SitesStages { get; set; } = new List<SiteStage>();
-
         public ICollection<Work> Works { get; set; } = new List<Work>();
     }
 }
