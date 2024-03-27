d23e47389a716fbfb11c651bf72c795c4e8a71e9
karinanikolova <lianna_kn@yahoo.com>
2024-03-27 18:46:58 +0200

    Created SiteServiceModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Work/SiteServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ConstructionSiteReportingSystem.Core.Models.Work
+{
+	public class SiteServiceModel
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+	}
+}
