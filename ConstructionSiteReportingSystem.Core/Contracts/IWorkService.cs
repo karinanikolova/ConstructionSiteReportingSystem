4ef6f890dcd85395b15fc06445428c5d02a49f1f
karinanikolova <lianna_kn@yahoo.com>
2024-03-27 18:44:35 +0200

    Added IWorkService.

---
+++ ConstructionSiteReportingSystem.Core/Contracts/IWorkService.cs
@@ -0,0 +1,17 @@
+﻿using ConstructionSiteReportingSystem.Core.Models.Work;
+
+namespace ConstructionSiteReportingSystem.Core.Contracts
+{
+	public interface IWorkService
+	{
+		Task<IEnumerable<SiteServiceModel>> GetAllSitesAsync();
+
+		Task<IEnumerable<WorkTypeServiceModel>> GetAllWorkTypesAsync();
+
+		Task<IEnumerable<StageServiceModel>> GetAllStagesAsync();
+
+		Task<IEnumerable<ContractorServiceModel>> GetAllContractorsAsync();
+
+		Task<IEnumerable<UnitServiceModel>> GetAllUnitsAsync();
+	}
+}
