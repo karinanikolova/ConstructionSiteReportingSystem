bd2af1f3465f24259988ec6735f7c58b4ea2ddfc
karinanikolova <lianna_kn@yahoo.com>
2024-03-27 18:46:48 +0200

    Created ContractorServiceModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Work/ContractorServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ConstructionSiteReportingSystem.Core.Models.Work
+{
+	public class ContractorServiceModel
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+	}
+}
