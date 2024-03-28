e12b28e61558b67ccdfe0dc9935a475d59ecd562
karinanikolova <lianna_kn@yahoo.com>
2024-03-28 17:21:38 +0200

    Implemented new methods in WorkService.

--- ConstructionSiteReportingSystem.Core/Services/WorkService.cs
+++ ConstructionSiteReportingSystem.Core/Services/WorkService.cs
@@ -3,6 +3,7 @@ using ConstructionSiteReportingSystem.Core.Models.Work;
 using ConstructionSiteReportingSystem.Infrastructure.Data.Models;
 using ConstructionSiteReportingSystem.Infrastructure.Data.Utilities.Contracts;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
 
 namespace ConstructionSiteReportingSystem.Core.Services
 {
@@ -68,6 +69,72 @@ namespace ConstructionSiteReportingSystem.Core.Services
 					Name = wt.Name
 				})
 				.ToListAsync();
+		}
+
+		public async Task<bool> DoesSiteExistAsync(int siteId)
+		{
+			return await _repository.AllReadOnly<Site>()
+				.AnyAsync(s => s.Id == siteId);
+		}
+
+		public async Task<bool> DoesWorkTypeExistAsync(int workTypeId)
+		{
+			return await _repository.AllReadOnly<WorkType>()
+				.AnyAsync(s => s.Id == workTypeId);
+		}
+
+		public async Task<bool> DoesStageExistAsync(int stageId)
+		{
+			return await _repository.AllReadOnly<Stage>()
+				.AnyAsync(s => s.Id == stageId);
+		}
+
+		public async Task<bool> DoesContractorExistAsync(int contractorId)
+		{
+			return await _repository.AllReadOnly<Contractor>()
+				.AnyAsync(s => s.Id == contractorId);
+		}
+
+		public async Task<bool> DoesUnitExistAsync(int unitId)
+		{
+			return await _repository.AllReadOnly<Unit>()
+				.AnyAsync(s => s.Id == unitId);
+		}
+
+		public async Task<int> CreateWorkAsync(WorkFormModel workModel, DateTime carryOutDate, string userId)
+		{
+			var work = new Work()
+			{
+				WorkTypeId = workModel.WorkTypeId,
+				Description = workModel.Description,
+				CarryOutDate = carryOutDate,
+				StageId = workModel.StageId,
+				ContractorId = workModel.ContractorId,
+				Quantity = workModel.Quantity,
+				UnitId = workModel.UnitId,
+				CostPerUnit = workModel.CostPerUnit,
+				TotalCost = CalculateTotalCost(workModel.Quantity, workModel.CostPerUnit),
+				CreatorId = userId
+			};
+
+			await _repository.AddAsync<Work>(work);
+			await _repository.SaveChangesAsync();
+
+			var siteWork = new SiteWork()
+			{
+				SiteId = workModel.SiteId,
+				WorkId = work.Id
+			};
+
+			await _repository.AddAsync<SiteWork>(siteWork);
+			await _repository.SaveChangesAsync();
+
+			return workModel.SiteId;
+		}
+
+		private decimal CalculateTotalCost(double quantity, decimal costPerUnit)
+		{
+			return (decimal)quantity * costPerUnit;
 		}
 	}
 }
