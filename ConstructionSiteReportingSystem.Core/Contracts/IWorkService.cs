39792cf180bace689a2a935bcf661d0d6a8cdcbd
karinanikolova <lianna_kn@yahoo.com>
2024-03-28 17:21:17 +0200

    Added methods to IWorkService interface.

--- ConstructionSiteReportingSystem.Core/Contracts/IWorkService.cs
+++ ConstructionSiteReportingSystem.Core/Contracts/IWorkService.cs
@@ -13,5 +13,17 @@ namespace ConstructionSiteReportingSystem.Core.Contracts
 		Task<IEnumerable<ContractorServiceModel>> GetAllContractorsAsync();
 
 		Task<IEnumerable<UnitServiceModel>> GetAllUnitsAsync();
+
+		Task<bool> DoesSiteExistAsync(int siteId);
+
+		Task<bool> DoesWorkTypeExistAsync(int workTypeId);
+
+		Task<bool> DoesStageExistAsync(int stageId);
+
+		Task<bool> DoesContractorExistAsync(int contractorId);
+
+		Task<bool> DoesUnitExistAsync(int unitId);
+
+		Task<int> CreateWorkAsync(WorkFormModel workModel, DateTime carryOutDate, string userId);
 	}
 }
