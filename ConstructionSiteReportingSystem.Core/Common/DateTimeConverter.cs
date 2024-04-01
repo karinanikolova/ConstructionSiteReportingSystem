8e168aa18247f4f5c1fcb6b5bfa1a3a15489eb72
karinanikolova <lianna_kn@yahoo.com>
2024-04-01 12:55:58 +0300

    Created DateTimeConverter static class to use for DateTime to string conversions across application.

---
+++ ConstructionSiteReportingSystem.Core/Common/DateTimeConverter.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using static ConstructionSiteReportingSystem.Core.Constants.ValidationConstants;
+
+namespace ConstructionSiteReportingSystem.Core.Common
+{
+	public static class DateTimeConverter
+	{
+		public static string ConvertDateToString(DateTime date)
+		{
+			return date.ToString(DateTimePreferredFormat, CultureInfo.InvariantCulture);
+		}
+	}
+}
