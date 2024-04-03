06947f0982f91fd9a17971cd3f529c2728e8b8d4
karinanikolova <lianna_kn@yahoo.com>
2024-04-03 11:17:16 +0300

    Added ClaimsPrincipalExtensions in order to use custom methods across application.

---
+++ ConstructionSIteReportingSystem/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace System.Security.Claims
+{
+	public static class ClaimsPrincipalExtensions
+	{
+		/// <summary>
+		/// Providing a method to be used across the application for retrieving the hashed user identifier as a string.
+		/// </summary>
+		/// <param name="user"></param>
+		/// <returns></returns>
+		public static string Id(this ClaimsPrincipal user)
+		{
+			return user.FindFirstValue(ClaimTypes.NameIdentifier);
+		}
+	}
+}
