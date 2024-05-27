f23c868a825e7320f4ded071dd1270af6935dc71
karinanikolova <lianna_kn@yahoo.com>
2024-05-27 18:23:06 +0300

    Created AllTasksQueryModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Task/AllTasksQueryModel.cs
@@ -0,0 +1,25 @@
+﻿using ConstructionSiteReportingSystem.Infrastructure.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConstructionSiteReportingSystem.Core.Models.Task
+{
+	public class AllTasksQueryModel
+	{
+		public const int TasksPerPage = 5;
+
+		public string Status { get; init; } = null!;
+
+		[Display(Name = "Search by text")]
+		public string? SearchTerm { get; init; }
+
+		public DateSorting Sorting { get; init; }
+
+		public int CurrentPage { get; init; } = 1;
+
+		public int TotalTasksCount { get; set; }
+
+		public IEnumerable<string> Statuses { get; set; } = new List<string>();
+
+		public IEnumerable<TaskViewModel> Tasks { get; set; } = new List<TaskViewModel>();
+	}
+}
