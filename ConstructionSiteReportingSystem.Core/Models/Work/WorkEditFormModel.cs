bde4f6ed68a5ef96283d50e9cd49d73fdac73022
karinanikolova <lianna_kn@yahoo.com>
2024-05-27 10:30:25 +0300

    Created WorkEditFormModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Work/WorkEditFormModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ConstructionSiteReportingSystem.Core.Models.Work
+{
+	public class WorkEditFormModel : WorkAddFormModel
+	{
+		public string CreatorId { get; set; } = string.Empty;
+	}
+}
