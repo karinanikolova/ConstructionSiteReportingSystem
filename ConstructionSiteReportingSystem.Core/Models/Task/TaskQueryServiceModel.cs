751c7c37fccbd00cc8a8f0986aef4785d718fa84
karinanikolova <lianna_kn@yahoo.com>
2024-05-27 18:22:46 +0300

    Created TaskQueryServiceModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Task/TaskQueryServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ConstructionSiteReportingSystem.Core.Models.Task
+{
+	public class TaskQueryServiceModel
+	{
+		public int TotalTasksCount { get; set; }
+
+		public IEnumerable<TaskViewModel> Tasks { get; set; } = new List<TaskViewModel>();
+	}
+}
