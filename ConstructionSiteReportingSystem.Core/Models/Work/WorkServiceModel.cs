07b5195c27e426fa207e96dee0a4002fcba3affb
karinanikolova <lianna_kn@yahoo.com>
2024-05-27 10:30:44 +0300

    Created WorkServiceModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Work/WorkServiceModel.cs
@@ -0,0 +1,29 @@
+﻿namespace ConstructionSiteReportingSystem.Core.Models.Work
+{
+	public class WorkServiceModel
+	{
+		public int Id { get; set; }
+
+		public string WorkType { get; set; } = string.Empty;
+
+		public string? Description { get; set; }
+
+		public string CarryOutDate { get; set; } = string.Empty;
+
+		public int SiteId { get; set; } //
+
+		public string Stage { get; set; } = string.Empty;
+
+		public string Contractor { get; set; } = string.Empty;
+
+		public double Quantity { get; set; }
+
+		public string Unit { get; set; } = string.Empty;
+
+		public decimal CostPerUnit { get; set; }
+
+		public decimal TotalCost { get; set; }
+
+		public string CreatorId { get; set; } = string.Empty; //
+	}
+}
