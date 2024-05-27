1a65c999e232084e98d94118ce7e146c254587e1
karinanikolova <lianna_kn@yahoo.com>
2024-05-27 18:22:19 +0300

    Created TaskViewModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Task/TaskViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ConstructionSiteReportingSystem.Infrastructure.Enums;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConstructionSiteReportingSystem.Core.Models.Task
+{
+	public class TaskViewModel
+	{
+		public int Id { get; set; }
+
+		public string Title { get; set; } = string.Empty;
+
+		public string Description { get; set; } = string.Empty;
+
+		public DateTime CreatedOn { get; set; }
+
+		public Status Status { get; set; }
+
+		public string Creator { get; set; } = string.Empty;
+	}
+}
