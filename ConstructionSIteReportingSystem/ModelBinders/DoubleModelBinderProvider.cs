1f205c4455f7eae499932a306a4aeb36caacb70a
karinanikolova <lianna_kn@yahoo.com>
2024-05-27 13:30:50 +0300

    Created DoubleModelBinderProvider.

---
+++ ConstructionSIteReportingSystem/ModelBinders/DoubleModelBinderProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ConstructionSiteReportingSystem.ModelBinders
+{
+	public class DoubleModelBinderProvider : IModelBinderProvider
+	{
+		public IModelBinder? GetBinder(ModelBinderProviderContext context)
+		{
+			if (context == null)
+			{
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			if (context.Metadata.ModelType == typeof(double)
+				|| context.Metadata.ModelType == typeof(double?))
+			{
+				return new DoubleModelBinder();
+			}
+
+			return null;
+		}
+	}
+}
