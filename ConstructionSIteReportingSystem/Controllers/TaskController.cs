95bf4d52e3bd751ecb905341b7f305fa87be860d
karinanikolova <lianna_kn@yahoo.com>
2024-05-28 17:54:41 +0300

    Added ITaskService to the TaskController and created All HttpGet action.

--- ConstructionSIteReportingSystem/Controllers/TaskController.cs
+++ ConstructionSIteReportingSystem/Controllers/TaskController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ConstructionSiteReportingSystem.Core.Models.Task;
+using ConstructionSiteReportingSystem.Core.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace ConstructionSiteReportingSystem.Controllers
 {
-    public class TaskController : BaseController
+	public class TaskController : BaseController
     {
-        public IActionResult Index()
+		private readonly ILogger<TaskController> _logger;
+		private readonly ITaskService _taskService;
+
+		public TaskController(ILogger<TaskController> logger, ITaskService taskService)
+		{
+			_logger = logger;
+			_taskService = taskService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> All([FromQuery] AllTasksQueryModel model)
         {
-            return View();
+			var userId = User.Id();
+
+			var tasks = await _taskService.GetAllUserTasksAsync(
+				userId,
+				model.Status,
+				model.SearchTerm,
+				model.Sorting,
+				model.CurrentPage,
+				AllTasksQueryModel.TasksPerPage);
+
+			model.TotalTasksCount = tasks.TotalTasksCount;
+			model.Tasks = tasks.Tasks;
+			model.Statuses = _taskService.GetAllStatusesAsString();
+
+            return View(model);
         }
+
+		
     }
 }
