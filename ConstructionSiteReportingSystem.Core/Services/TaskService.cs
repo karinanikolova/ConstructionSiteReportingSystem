087c0d196e9e46c43ded1c349148765789d678d8
karinanikolova <lianna_kn@yahoo.com>
2024-05-30 15:28:29 +0300

    Defined new methods for adding and editing functionality of Tasks in TaskService.

--- ConstructionSiteReportingSystem.Core/Services/TaskService.cs
+++ ConstructionSiteReportingSystem.Core/Services/TaskService.cs
@@ -54,7 +54,7 @@ namespace ConstructionSiteReportingSystem.Core.Services
 					Title = t.Title,
 					Description = t.Description,
 					CreatedOn = t.CreatedOn,
-					Status = t.Status/*.ToString() == "NotStarted" ? "Not started" : t.Status.ToString() == "InProgress" ? "In progress" : t.Status.ToString()*/,
+					Status = t.Status,
 					Creator = t.CreatorId
 				})
 				.ToListAsync();
@@ -68,13 +68,78 @@ namespace ConstructionSiteReportingSystem.Core.Services
 			};
 		}
 
-		public IEnumerable<string> GetAllStatusesAsString() =>
-			new List<string>()
+		public IEnumerable<string> GetAllStatusesAsString() => new List<string>()
 		{
 			Status.NotStarted.ToString(),
 			Status.InProgress.ToString(),
 			Status.Finished.ToString()
 		};
 
+		public IEnumerable<int> GetAllStatusesAsInt() => new List<int>()
+		{
+			(int)Status.NotStarted,
+			(int)Status.InProgress,
+			(int)Status.Finished
+		};
+
+		public bool DoesStatusExist(int statusId)
+		{
+			var statuses = GetAllStatusesAsInt();
+
+			return statuses.Any(s => s == statusId);
+		}
+
+		public async System.Threading.Tasks.Task CreateTaskAsync(TaskAddFormModel taskModel, string userId)
+		{
+			var task = new Task()
+			{
+				Title = taskModel.Title,
+				Description = taskModel.Description,
+				CreatedOn = DateTime.UtcNow,
+				CreatorId = userId,
+				Status = (Status)taskModel.StatusId
+			};
+
+			await _repository.AddAsync<Task>(task);
+			await _repository.SaveChangesAsync();
+		}
+
+		public async Task<bool> DoesTaskExistAsync(int taskId) => await _repository.AllReadOnly<Task>()
+				.AnyAsync(w => w.Id == taskId);
+
+		public async Task<TaskEditFormModel?> GetTaskEditFormModelByIdAsync(int taskId)
+		{
+			var task = await _repository.AllReadOnly<Task>()
+				.Where(t => t.Id == taskId)
+				.Select(t => new TaskEditFormModel()
+				{
+					Title = t.Title,
+					Description = t.Description,
+					StatusId = (int)t.Status,
+					CreatorId = t.CreatorId
+				})
+				.FirstOrDefaultAsync();
+
+			if (task != null)
+			{
+				task.Statuses = GetAllStatusesAsInt();
+			}
+
+			return task;
+		}
+
+		public async System.Threading.Tasks.Task EditTaskAsync(int taskId, TaskEditFormModel taskModel)
+		{
+			var task = await _repository.GetByIdAsync<Task>(taskId);
+
+			if (task != null)
+			{
+				task.Title = taskModel.Title;
+				task.Description = taskModel.Description;
+				task.Status = (Status)taskModel.StatusId;
+			}
+
+			await _repository.SaveChangesAsync();
+		}
 	}
 }
