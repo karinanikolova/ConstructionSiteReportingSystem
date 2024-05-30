d0f683ed6afa0f7401a47bea882ac7a15e3b8b16
karinanikolova <lianna_kn@yahoo.com>
2024-05-30 15:27:34 +0300

    Added new methods for adding and editing functionality of Tasks to ITaskService.

--- ConstructionSiteReportingSystem.Core/Services/Contracts/ITaskService.cs
+++ ConstructionSiteReportingSystem.Core/Services/Contracts/ITaskService.cs
@@ -8,5 +8,17 @@ namespace ConstructionSiteReportingSystem.Core.Services.Contracts
         Task<TaskQueryServiceModel> GetAllUserTasksAsync(string userId, string? searchStatus = null, string? searchTerm = null, DateSorting dateSorting = DateSorting.Newest, int currentPage = 1, int tasksPerPage = 1);
 
         IEnumerable<string> GetAllStatusesAsString();
-    }
+
+		IEnumerable<int> GetAllStatusesAsInt();
+
+		Task<TaskEditFormModel?> GetTaskEditFormModelByIdAsync(int taskId);
+
+		bool DoesStatusExist(int statusId);
+
+		Task CreateTaskAsync(TaskAddFormModel taskModel, string userId);
+
+		Task<bool> DoesTaskExistAsync(int taskId);
+
+		Task EditTaskAsync(int taskId, TaskEditFormModel taskModel);
+	}
 }
