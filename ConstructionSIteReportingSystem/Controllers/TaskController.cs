5512a38ef0e8148840cf70509b273670d5d73bea
karinanikolova <lianna_kn@yahoo.com>
2024-05-30 15:30:22 +0300

    Created AddTask and EditTask HttpGet and HttpPost actions in TaskController.

--- ConstructionSIteReportingSystem/Controllers/TaskController.cs
+++ ConstructionSIteReportingSystem/Controllers/TaskController.cs
@@ -36,6 +36,87 @@ namespace ConstructionSiteReportingSystem.Controllers
             return View(model);
         }
 
-		
-    }
+		[HttpGet]
+		public async Task<IActionResult> AddTask()
+		{
+			var statuses = _taskService.GetAllStatusesAsInt();
+
+			var taskModel = new TaskAddFormModel()
+			{
+				Statuses = statuses
+			};
+
+			return View(taskModel);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> AddTask(TaskAddFormModel taskModel)
+		{
+			if (_taskService.DoesStatusExist(taskModel.StatusId) == false)
+			{
+				ModelState.AddModelError(nameof(taskModel.StatusId), "Task status does not exist");
+			}
+
+			if (!ModelState.IsValid)
+			{
+				taskModel.Statuses = _taskService.GetAllStatusesAsInt();
+
+				return View(taskModel);
+			}
+
+			var userId = User.Id();
+
+			await _taskService.CreateTaskAsync(taskModel, userId);
+
+			return RedirectToAction(nameof(All));
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> EditTask(int id)
+		{
+			if (await _taskService.DoesTaskExistAsync(id) == false)
+			{
+				return BadRequest();
+			}
+
+			var task = await _taskService.GetTaskEditFormModelByIdAsync(id);
+
+			if (User.Id() != task!.CreatorId)
+			{
+				return Unauthorized();
+			}
+
+			return View(task);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> EditTask(int id, TaskEditFormModel taskModel)
+		{
+			if (await _taskService.DoesTaskExistAsync(id) == false)
+			{
+				return BadRequest();
+			}
+
+			if (User.Id() != taskModel!.CreatorId)
+			{
+				return Unauthorized();
+			}
+
+			if (_taskService.DoesStatusExist(taskModel.StatusId) == false)
+			{
+				ModelState.AddModelError(nameof(taskModel.StatusId), "Task status does not exist");
+			}
+
+			if (!ModelState.IsValid)
+			{
+				taskModel.Statuses = _taskService.GetAllStatusesAsInt();
+
+				return View(taskModel);
+			}
+
+			await _taskService.EditTaskAsync(id, taskModel);
+
+			return RedirectToAction(nameof(All));
+		}
+	}
 }
