ad358d46ef0b5e13d1ed115d3a7d9c4986e23d25
karinanikolova <lianna_kn@yahoo.com>
2024-05-30 15:26:02 +0300

    Created TaskEditFormModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Task/TaskEditFormModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ConstructionSiteReportingSystem.Core.Models.Task
+{
+	public class TaskEditFormModel : TaskAddFormModel
+	{
+		public string CreatorId { get; set; } = string.Empty;
+	}
+}
