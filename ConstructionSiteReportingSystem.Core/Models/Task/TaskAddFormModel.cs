5ee9a88ffb6aeb6be8ce4f4c2f69653b5675e775
karinanikolova <lianna_kn@yahoo.com>
2024-05-30 15:25:49 +0300

    Created TaskAddFormModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Task/TaskAddFormModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ConstructionSiteReportingSystem.Core.Constants.MessageConstants;
+using static ConstructionSiteReportingSystem.Infrastructure.Constants.DataConstants.Task;
+
+namespace ConstructionSiteReportingSystem.Core.Models.Task
+{
+	public class TaskAddFormModel
+	{
+		[Required(ErrorMessage = RequiredFieldMessage)]
+		[StringLength(TitleMaxLength, MinimumLength = TitleMinLength, ErrorMessage = TaskFieldLengthMessage)]
+		public string Title { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = RequiredFieldMessage)]
+		[StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength, ErrorMessage = TaskFieldLengthMessage)]
+		public string Description { get; set; } = string.Empty;
+
+		[Display(Name = "Status")]
+		public int StatusId { get; set; }
+
+		public IEnumerable<int> Statuses { get; set; } = new List<int>();
+	}
+}
