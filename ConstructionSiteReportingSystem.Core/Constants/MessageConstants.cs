e586bc06c473d08a1f99061335369304dcc66b0e
karinanikolova <lianna_kn@yahoo.com>
2024-05-30 15:25:17 +0300

    Added TaskFieldLengthMessage property to MessageConstants.

--- ConstructionSiteReportingSystem.Core/Constants/MessageConstants.cs
+++ ConstructionSiteReportingSystem.Core/Constants/MessageConstants.cs
@@ -11,5 +11,7 @@
         public const string QuantityMessage = "{0} must be a whole positive number bigger than zero";
 
         public const string CostPerUnitMessage = "{0} must be a positive number bigger than zero";
+
+        public const string TaskFieldLengthMessage = "Field {0} must contain between {2} and {1} characters";
 	}
 }
