efa18ad53b5e6cb027f9ea14fdfdc5cf98a9e339
karinanikolova <lianna_kn@yahoo.com>
2024-06-11 17:13:08 +0300

    Created ISiteModel interface.

---
+++ ConstructionSiteReportingSystem.Core/Extensions/Contracts/ISiteModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ConstructionSiteReportingSystem.Core.Extensions.Contracts
+{
+    /// <summary>
+    /// Interface which can be implemented by Model classes in order to have properties for modifying page URLs.
+    /// </summary>
+    public interface ISiteModel
+    {
+        /// <summary>
+        /// Represents the name of the Model class.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Represents the finish date of the Model class.
+        /// </summary>
+        public string FinishDate { get; }
+    }
+}
