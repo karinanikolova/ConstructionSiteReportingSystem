f515eedf09adfe6f792741f0b1e56aa2d5c3d2d8
karinanikolova <lianna_kn@yahoo.com>
2024-06-11 17:13:45 +0300

    Created ModelExtensions static class.

---
+++ ConstructionSiteReportingSystem.Core/Extensions/ModelExtensions.cs
@@ -0,0 +1,23 @@
+﻿using ConstructionSiteReportingSystem.Core.Extensions.Contracts;
+using System.Text.RegularExpressions;
+
+namespace ConstructionSiteReportingSystem.Core.Extensions
+{
+    public static class ModelExtensions
+    {
+        public static string GetInformation(this ISiteModel site)
+        {
+            string info = site.Name.Replace(" ", "-") + GetFinishDate(site.FinishDate);
+            info = Regex.Replace(info, @"[^a-zA-Z0-9\-]", string.Empty);
+
+            return info;
+        }
+
+        private static string GetFinishDate(string finishDate)
+        {
+            finishDate = string.Join("-", finishDate.Split(" ").Take(3));
+
+            return finishDate;
+        }
+    }
+}
