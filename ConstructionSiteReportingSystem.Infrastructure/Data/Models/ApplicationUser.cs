4783d0f1c0e713c6fa60c35db48e3297231df1d4
karinanikolova <lianna_kn@yahoo.com>
2024-06-27 13:55:53 +0300

    Created ApplicationUser entity class in order to extend the default IdentityUser.

---
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Models/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using static ConstructionSiteReportingSystem.Infrastructure.Constants.DataConstants.ApplicationUser;
+
+namespace ConstructionSiteReportingSystem.Infrastructure.Data.Models
+{
+	public class ApplicationUser : IdentityUser
+	{
+		[Required]
+		[MaxLength(FirstNameMaxLength)]
+		[PersonalData]
+		public string FirstName { get; set; } = string.Empty;
+
+		[Required]
+		[MaxLength(MiddleNameMaxLength)]
+        [PersonalData]
+        public string MiddleName { get; set; } = string.Empty;
+
+		[Required]
+		[MaxLength(LastNameMaxLength)]
+        [PersonalData]
+        public string LastName { get; set; } = string.Empty;
+	}
+}
