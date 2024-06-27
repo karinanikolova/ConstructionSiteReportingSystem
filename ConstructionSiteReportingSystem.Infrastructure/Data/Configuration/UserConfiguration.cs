835942a1859c18ee22f85b81c78a4faedb9bfa26
karinanikolova <lianna_kn@yahoo.com>
2024-06-27 13:59:08 +0300

    Changed IdentityUser to ApplicationUser in UserConfiguration class.

--- ConstructionSiteReportingSystem.Infrastructure/Data/Configuration/UserConfiguration.cs
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Configuration/UserConfiguration.cs
@@ -1,17 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using ConstructionSiteReportingSystem.Infrastructure.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ConstructionSiteReportingSystem.Infrastructure.Data.Configuration
 {
-    internal class UserConfiguration : IEntityTypeConfiguration<IdentityUser>
+	internal class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
     {
-        public void Configure(EntityTypeBuilder<IdentityUser> builder)
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
         {
-            // Seeding data for IdentityUsers
+            // Seeding data for ApplicationUsers
             var data = new DataSeed();
 
-            builder.HasData(new List<IdentityUser>(data.Users));
+            builder.HasData(new List<ApplicationUser>(data.Users));
         }
     }
 }
