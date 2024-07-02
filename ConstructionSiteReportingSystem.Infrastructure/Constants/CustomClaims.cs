2f04a19a670c27aca523d2856818e33fdfa87b5a
karinanikolova <lianna_kn@yahoo.com>
2024-07-02 17:22:26 +0300

    Added CustomClaims static class for holding claims constants.

---
+++ ConstructionSiteReportingSystem.Infrastructure/Constants/CustomClaims.cs
@@ -0,0 +1,7 @@
+﻿namespace ConstructionSiteReportingSystem.Infrastructure.Constants
+{
+    public static class CustomClaims
+    {
+        public const string UserFirstLastNameClaim = "user:firstlastname";
+    }
+}
