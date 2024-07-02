14d7da1170bae84d2ff33711fe08b0e34eb40fee
karinanikolova <lianna_kn@yahoo.com>
2024-07-02 17:23:53 +0300

    Added UserClaimsConfiguration for configuring IdentityUserClaim<string> seed.

---
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Configuration/UserClaimsConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ConstructionSiteReportingSystem.Infrastructure.Data.Configuration
+{
+    internal class UserClaimsConfiguration : IEntityTypeConfiguration<IdentityUserClaim<string>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUserClaim<string>> builder)
+        {
+            // Seeding data for IdentityUserClaim<string>
+            var data = new DataSeed();
+
+            builder.HasData(new List<IdentityUserClaim<string>>(data.UserClaims));
+        }
+    }
+}
