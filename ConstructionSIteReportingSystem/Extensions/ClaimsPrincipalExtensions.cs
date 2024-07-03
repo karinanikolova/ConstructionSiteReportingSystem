df2af89904eb19f7e1b983fd0a14dc35298ffe32
karinanikolova <lianna_kn@yahoo.com>
2024-07-03 16:28:01 +0300

    Implemented IsAdmin method in ClaimsPrincipalExtensions.

--- ConstructionSIteReportingSystem/Extensions/ClaimsPrincipalExtensions.cs
+++ ConstructionSIteReportingSystem/Extensions/ClaimsPrincipalExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace System.Security.Claims
+﻿using static ConstructionSiteReportingSystem.Core.Constants.AdministratorConstants;
+
+namespace System.Security.Claims
 {
 	public static class ClaimsPrincipalExtensions
 	{
@@ -10,6 +12,16 @@
 		public static string Id(this ClaimsPrincipal user)
 		{
 			return user.FindFirstValue(ClaimTypes.NameIdentifier);
+		}
+
+        /// <summary>
+        /// Providing a method to be used across the application for checking whether the current user has the administrator role assigned.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public static bool IsAdmin(this ClaimsPrincipal user)
+		{
+			return user.IsInRole(AdminRole);
 		}
 	}
 }
