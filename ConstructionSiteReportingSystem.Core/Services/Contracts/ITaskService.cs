1017a634ea5584537660c2e0438526fc88524833
karinanikolova <lianna_kn@yahoo.com>
2024-07-03 16:32:25 +0300

    Modified GetAllUserTasksAsync method in ITaskService interface.

--- ConstructionSiteReportingSystem.Core/Services/Contracts/ITaskService.cs
+++ ConstructionSiteReportingSystem.Core/Services/Contracts/ITaskService.cs
@@ -5,7 +5,7 @@ namespace ConstructionSiteReportingSystem.Core.Services.Contracts
 {
 	public interface ITaskService
     {
-        Task<TaskQueryServiceModel> GetAllUserTasksAsync(string userId, string? searchStatus = null, string? searchTerm = null, DateSorting dateSorting = DateSorting.Newest, int currentPage = 1, int tasksPerPage = 1);
+        Task<TaskQueryServiceModel> GetAllUserTasksAsync(string userId, bool isAdmin, string? searchStatus = null, string? searchTerm = null, DateSorting dateSorting = DateSorting.Newest, int currentPage = 1, int tasksPerPage = 1);
 
         IEnumerable<string> GetAllStatusesAsString();
 
