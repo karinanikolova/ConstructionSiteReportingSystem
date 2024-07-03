d86d448dfa638d3e5792f58948da6629d075bf87
karinanikolova <lianna_kn@yahoo.com>
2024-07-03 14:14:35 +0300

    Created AdministratorConstants static class to hold administrator constants.

---
+++ ConstructionSiteReportingSystem.Core/Constants/AdministratorConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace ConstructionSiteReportingSystem.Core.Constants
+{
+    public static class AdministratorConstants
+    {
+        public const string AdminRole = "Administrator";
+
+        public const string AdminEmail = "admin@mail.com";
+    }
+}
