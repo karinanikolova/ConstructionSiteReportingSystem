d258f5abc768787241c3a9a4743f9ca99173741a
karinanikolova <lianna_kn@yahoo.com>
2024-07-08 16:17:09 +0300

    Created HomeController for the admin area.

---
+++ ConstructionSIteReportingSystem/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConstructionSiteReportingSystem.Areas.Admin.Controllers
+{
+    public class HomeController : AdminBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ForReview()
+        {
+            return View();
+        }
+    }
+}
