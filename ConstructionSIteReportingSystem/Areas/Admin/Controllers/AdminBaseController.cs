a6c87fb6df7532c5263d3e94f043f37724b16277
karinanikolova <lianna_kn@yahoo.com>
2024-07-08 16:16:44 +0300

    Created AdminBaseController.

---
+++ ConstructionSIteReportingSystem/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static ConstructionSiteReportingSystem.Core.Constants.AdministratorConstants;
+
+namespace ConstructionSiteReportingSystem.Areas.Admin.Controllers
+{
+    [Area(AdminAreaName)]
+    [Authorize(Roles = AdminRole)]
+    public class AdminBaseController : Controller
+    {
+
+    }
+}
