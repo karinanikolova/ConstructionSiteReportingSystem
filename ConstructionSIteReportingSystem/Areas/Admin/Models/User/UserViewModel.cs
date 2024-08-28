1e3500f3174de765cbe7906256c05a35e62a9ffd
karinanikolova <lianna_kn@yahoo.com>
2024-08-28 16:45:37 +0300

    Created UserViewModel for the admin area.

---
+++ ConstructionSIteReportingSystem/Areas/Admin/Models/User/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ConstructionSiteReportingSystem.Areas.Admin.Models.User
+{
+	public class UserViewModel
+	{
+		public string Email { get; set; } = string.Empty;
+
+		public string FirstName { get; set; } = string.Empty;
+
+		public string MiddleName { get; set; } = string.Empty;
+
+		public string LastName { get; set; } = string.Empty;
+
+		public bool IsAdmin { get; set; }
+	}
+}
