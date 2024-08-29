299d1d85110a2820e7f70222754069fc20311f82
karinanikolova <lianna_kn@yahoo.com>
2024-08-29 15:11:23 +0300

    Moved UserViewModel to Models/Admin/User directory in ConstructionSiteReportingSystem.Core project file.

---
+++ ConstructionSiteReportingSystem.Core/Models/Admin/User/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ConstructionSiteReportingSystem.Core.Models.Admin.User
+{
+	public class UserViewModel
+	{
+		public string Id { get; set; } = null!;
+
+		public string Email { get; set; } = string.Empty;
+
+		public string FirstName { get; set; } = string.Empty;
+
+		public string MiddleName { get; set; } = string.Empty;
+
+		public string LastName { get; set; } = string.Empty;
+
+		public bool IsAdmin { get; set; }
+	}
+}
