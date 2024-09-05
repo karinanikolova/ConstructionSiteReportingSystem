4bac2f4ad6ed8eebc4899a708f41dd802266d699
karinanikolova <lianna_kn@yahoo.com>
2024-09-05 17:39:47 +0300

    Created ForReviewViewModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Admin/Index/ForReviewViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ConstructionSiteReportingSystem.Core.Models.Admin.Index
+{
+	public class ForReviewViewModel
+	{
+		public bool AreThereContractorsForReview { get; set; }
+
+		public bool AreThereStagesForReview { get; set; }
+
+		public bool AreThereUnitsForReview { get; set; }
+
+		public bool AreThereWorkTypesForReview { get; set; }
+	}
+}
