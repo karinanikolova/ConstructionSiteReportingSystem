0bab6b98cfee2327205618f6b6c2c4c3337cd108
karinanikolova <lianna_kn@yahoo.com>
2024-09-05 17:41:02 +0300

    Created ForReviewController.

---
+++ ConstructionSIteReportingSystem/Areas/Admin/Controllers/ForReviewController.cs
@@ -0,0 +1,43 @@
+﻿using ConstructionSiteReportingSystem.Core.Models.Work;
+using ConstructionSiteReportingSystem.Core.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConstructionSiteReportingSystem.Areas.Admin.Controllers
+{
+	public class ForReviewController : AdminBaseController
+	{
+		private readonly ILogger<ForReviewController> _logger;
+		private readonly IForReviewService _forReviewService;
+
+		public ForReviewController(ILogger<ForReviewController> logger, IForReviewService forReviewService)
+		{
+			_logger = logger;
+			_forReviewService = forReviewService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> ApproveContractors()
+		{
+			var contractorModels = await _forReviewService.GetContractorsForReviewAsync();
+
+			if (contractorModels == null)
+			{
+				return BadRequest();
+			}
+
+			return View(contractorModels);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> ApproveContractor(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> RemoveContractor(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
