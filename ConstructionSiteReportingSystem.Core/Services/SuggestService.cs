a4cfd02036bdfe53e3082257afd603bca8c2d660
karinanikolova <lianna_kn@yahoo.com>
2024-09-05 17:35:48 +0300

    Created SuggestService class.

---
+++ ConstructionSiteReportingSystem.Core/Services/SuggestService.cs
@@ -0,0 +1,29 @@
+﻿using ConstructionSiteReportingSystem.Core.Models.Suggest;
+using ConstructionSiteReportingSystem.Core.Services.Contracts;
+using ConstructionSiteReportingSystem.Infrastructure.Data.Utilities.Contracts;
+using Contractor = ConstructionSiteReportingSystem.Infrastructure.Data.Models.Contractor;
+
+namespace ConstructionSiteReportingSystem.Core.Services
+{
+	public class SuggestService : ISuggestService
+	{
+		private readonly IRepository _repository;
+
+        public SuggestService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task CreateContractorAsync(ContractorAddFormModel contractorModel, bool isUserAdmin)
+		{
+			var contractor = new Contractor()
+			{
+				Name = contractorModel.Name,
+				IsApproved = isUserAdmin ? true : false
+			};
+
+			await _repository.AddAsync<Contractor>(contractor);
+			await _repository.SaveChangesAsync();
+		}
+	}
+}
