293b3a73e71594079bacc83bea1caaf55984d9a6
karinanikolova <lianna_kn@yahoo.com>
2024-09-05 17:24:34 +0300

    Added IsApproved property to Stage data model.

--- ConstructionSiteReportingSystem.Infrastructure/Data/Models/Stage.cs
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Models/Stage.cs
@@ -17,6 +17,10 @@ namespace ConstructionSiteReportingSystem.Infrastructure.Data.Models
         [Comment("Construction stage name")]
         public string Name { get; set; } = string.Empty;
 
-        public ICollection<Work> Works { get; set; } = new List<Work>();
+		[Required]
+		[Comment("Is construction stage approved by the administrator")]
+		public bool IsApproved { get; set; }
+
+		public ICollection<Work> Works { get; set; } = new List<Work>();
     }
 }
