47e78ba5b14e5738dc32029f9a3ec3fb2a7826f0
karinanikolova <lianna_kn@yahoo.com>
2024-09-05 17:34:55 +0300

    Created IForReviewService interface.

---
+++ ConstructionSiteReportingSystem.Core/Services/Contracts/IForReviewService.cs
@@ -0,0 +1,18 @@
+﻿using ConstructionSiteReportingSystem.Core.Models.Admin.Index;
+using ConstructionSiteReportingSystem.Core.Models.Work;
+
+namespace ConstructionSiteReportingSystem.Core.Services.Contracts
+{
+	public interface IForReviewService
+    {
+        Task<ForReviewViewModel> GetForReviewViewModelAsync();
+
+		Task<IEnumerable<ContractorServiceModel>> GetContractorsForReviewAsync();
+
+		Task<IEnumerable<StageServiceModel>> GetStagesForReviewAsync();
+
+		Task<IEnumerable<UnitServiceModel>> GetUnitsForReviewAsync();
+
+		Task<IEnumerable<WorkTypeServiceModel>> GetWorkTypesForReviewAsync();
+	}
+}
