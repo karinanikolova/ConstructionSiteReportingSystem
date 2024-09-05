35136583874139b8d01974d015b25831f8fe3dd5
karinanikolova <lianna_kn@yahoo.com>
2024-09-05 17:35:10 +0300

    Created ISuggestService interface.

---
+++ ConstructionSiteReportingSystem.Core/Services/Contracts/ISuggestService.cs
@@ -0,0 +1,9 @@
+﻿using ConstructionSiteReportingSystem.Core.Models.Suggest;
+
+namespace ConstructionSiteReportingSystem.Core.Services.Contracts
+{
+	public interface ISuggestService
+    {
+        Task CreateContractorAsync(ContractorAddFormModel contractorModel, bool isUserAdmin);
+    }
+}
