31a7947949a45e85985e731a07e379d9d3cfdf4d
karinanikolova <lianna_kn@yahoo.com>
2024-09-05 17:06:09 +0300

    Modified Index action method and injected logger and service in HomeController of the admin area.

--- ConstructionSIteReportingSystem/Areas/Admin/Controllers/HomeController.cs
+++ ConstructionSIteReportingSystem/Areas/Admin/Controllers/HomeController.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ConstructionSiteReportingSystem.Core.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ConstructionSiteReportingSystem.Areas.Admin.Controllers
 {
-    public class HomeController : AdminBaseController
+	public class HomeController : AdminBaseController
     {
-        [HttpGet]
-        public IActionResult Index()
+		private readonly ILogger<ForReviewController> _logger;
+		private readonly IForReviewService _forReviewService;
+
+        public HomeController(ILogger<ForReviewController> logger, IForReviewService forReviewService)
         {
-            return View();
+			_logger = logger;
+			_forReviewService = forReviewService;
+		}
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var forReviewViewModel = await _forReviewService.GetForReviewViewModelAsync();
+
+            return View(forReviewViewModel);
         }
     }
 }
