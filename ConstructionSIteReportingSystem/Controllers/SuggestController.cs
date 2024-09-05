076349bc6a3afb415b6dd9a888b8e663e966d4e6
karinanikolova <lianna_kn@yahoo.com>
2024-09-05 17:40:37 +0300

    Created SuggestController..

---
+++ ConstructionSIteReportingSystem/Controllers/SuggestController.cs
@@ -0,0 +1,40 @@
+﻿using ConstructionSiteReportingSystem.Core.Models.Suggest;
+using ConstructionSiteReportingSystem.Core.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace ConstructionSiteReportingSystem.Controllers
+{
+	public class SuggestController : BaseController
+	{
+		private readonly ILogger<SuggestController> _logger;
+		private readonly ISuggestService _suggestService;
+
+        public SuggestController(ILogger<SuggestController> logger, ISuggestService suggestService)
+        {
+			_suggestService = suggestService;
+			_logger = logger;
+        }
+
+        [HttpGet]
+		public async Task<IActionResult> Contractor()
+		{
+			return View(new ContractorAddFormModel());
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Contractor(ContractorAddFormModel contractorModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(contractorModel);
+			}
+
+			bool isUserAdmin = User.IsAdmin();
+
+			await _suggestService.CreateContractorAsync(contractorModel, isUserAdmin);
+
+			return RedirectToAction("All", "ConstructionSite");
+		}
+	}
+}
