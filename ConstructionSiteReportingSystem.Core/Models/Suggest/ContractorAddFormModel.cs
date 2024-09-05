d36cff66971bb66bacb0e73bd25313ffbe797b26
karinanikolova <lianna_kn@yahoo.com>
2024-09-05 17:40:10 +0300

    Created ContractorAddFormModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Suggest/ContractorAddFormModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ConstructionSiteReportingSystem.Core.Constants.MessageConstants;
+using static ConstructionSiteReportingSystem.Infrastructure.Constants.DataConstants.Contractor;
+
+namespace ConstructionSiteReportingSystem.Core.Models.Suggest
+{
+	public class ContractorAddFormModel
+	{
+		[Required(ErrorMessage = RequiredFieldMessage)]
+		[StringLength(NameMaxLength, MinimumLength = NameMinLength, ErrorMessage = ContractorFieldLengthMessage)]
+		public string Name { get; set; } = string.Empty;
+	}
+}
