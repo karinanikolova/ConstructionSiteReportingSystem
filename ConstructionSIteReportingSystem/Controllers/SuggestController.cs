6e3ce35388e6e13cf9b3c20f89939b4c7e2c0416
karinanikolova <lianna_kn@yahoo.com>
2024-09-06 20:14:44 +0300

    Added action methods in SuggestController.

--- ConstructionSIteReportingSystem/Controllers/SuggestController.cs
+++ ConstructionSIteReportingSystem/Controllers/SuggestController.cs
@@ -36,5 +36,68 @@ namespace ConstructionSiteReportingSystem.Controllers
 
 			return RedirectToAction("All", "ConstructionSite");
 		}
+
+		[HttpGet]
+		public async Task<IActionResult> Stage()
+		{
+			return View(new StageAddFormModel());
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Stage(StageAddFormModel stageModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(stageModel);
+			}
+
+			bool isUserAdmin = User.IsAdmin();
+
+			await _suggestService.CreateStageAsync(stageModel, isUserAdmin);
+
+			return RedirectToAction("All", "ConstructionSite");
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Unit()
+		{
+			return View(new UnitAddFormModel());
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Unit(UnitAddFormModel unitModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(unitModel);
+			}
+
+			bool isUserAdmin = User.IsAdmin();
+
+			await _suggestService.CreateUnitAsync(unitModel, isUserAdmin);
+
+			return RedirectToAction("All", "ConstructionSite");
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> WorkType()
+		{
+			return View(new WorkTypeAddFormModel());
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> WorkType(WorkTypeAddFormModel workTypeModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(workTypeModel);
+			}
+
+			bool isUserAdmin = User.IsAdmin();
+
+			await _suggestService.CreateWorkTypeAsync(workTypeModel, isUserAdmin);
+
+			return RedirectToAction("All", "ConstructionSite");
+		}
 	}
 }
