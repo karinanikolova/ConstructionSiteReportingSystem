5968cebba212be2816cc84981cc2c78edc4441b8
karinanikolova <lianna_kn@yahoo.com>
2024-09-06 20:08:29 +0300

    Created UnitAddFormModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Suggest/UnitAddFormModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ConstructionSiteReportingSystem.Core.Constants.MessageConstants;
+using static ConstructionSiteReportingSystem.Infrastructure.Constants.DataConstants.Unit;
+
+namespace ConstructionSiteReportingSystem.Core.Models.Suggest
+{
+	public class UnitAddFormModel
+	{
+		[Required(ErrorMessage = RequiredFieldMessage)]
+		[StringLength(TypeMaxLength, MinimumLength = TypeMinLength, ErrorMessage = FieldLengthMessage)]
+		public string Type { get; set; } = string.Empty;
+	}
+}
