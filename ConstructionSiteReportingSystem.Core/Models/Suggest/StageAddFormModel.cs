7658a9cabec7b5ac987fae1cd13419e8c5a29cc0
karinanikolova <lianna_kn@yahoo.com>
2024-09-06 20:08:10 +0300

    Created StageAddFormModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Suggest/StageAddFormModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ConstructionSiteReportingSystem.Core.Constants.MessageConstants;
+using static ConstructionSiteReportingSystem.Infrastructure.Constants.DataConstants.Stage;
+
+namespace ConstructionSiteReportingSystem.Core.Models.Suggest
+{
+	public class StageAddFormModel
+	{
+		[Required(ErrorMessage = RequiredFieldMessage)]
+		[StringLength(NameMaxLength, MinimumLength = NameMinLength, ErrorMessage = FieldLengthMessage)]
+		public string Name { get; set; } = string.Empty;
+	}
+}
