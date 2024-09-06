4d0bc02588fe2e4fc73a0f4a2071db12045fc7d3
karinanikolova <lianna_kn@yahoo.com>
2024-09-06 20:08:48 +0300

    Created WorkTypeAddFormModel.

---
+++ ConstructionSiteReportingSystem.Core/Models/Suggest/WorkTypeAddFormModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ConstructionSiteReportingSystem.Core.Constants.MessageConstants;
+using static ConstructionSiteReportingSystem.Infrastructure.Constants.DataConstants.WorkType;
+
+namespace ConstructionSiteReportingSystem.Core.Models.Suggest
+{
+	public class WorkTypeAddFormModel
+	{
+		[Required(ErrorMessage = RequiredFieldMessage)]
+		[StringLength(NameMaxLength, MinimumLength = NameMinLength, ErrorMessage = FieldLengthMessage)]
+		public string Name { get; set; } = string.Empty;
+	}
+}
