a2cce4d6ad16b1378ccf055221f699a273e99e46
karinanikolova <lianna_kn@yahoo.com>
2024-09-06 20:12:44 +0300

    Implemented new methods in SuggestService.

--- ConstructionSiteReportingSystem.Core/Services/SuggestService.cs
+++ ConstructionSiteReportingSystem.Core/Services/SuggestService.cs
@@ -1,7 +1,8 @@
 ﻿using ConstructionSiteReportingSystem.Core.Models.Suggest;
 using ConstructionSiteReportingSystem.Core.Services.Contracts;
+using ConstructionSiteReportingSystem.Infrastructure.Data.Models;
 using ConstructionSiteReportingSystem.Infrastructure.Data.Utilities.Contracts;
-using Contractor = ConstructionSiteReportingSystem.Infrastructure.Data.Models.Contractor;
+using Task = System.Threading.Tasks.Task;
 
 namespace ConstructionSiteReportingSystem.Core.Services
 {
@@ -23,6 +24,42 @@ namespace ConstructionSiteReportingSystem.Core.Services
 			};
 
 			await _repository.AddAsync<Contractor>(contractor);
+			await _repository.SaveChangesAsync();
+		}
+
+		public async Task CreateStageAsync(StageAddFormModel stageModel, bool isUserAdmin)
+		{
+			var stage = new Stage()
+			{
+				Name = stageModel.Name,
+				IsApproved = isUserAdmin ? true : false
+			};
+
+			await _repository.AddAsync<Stage>(stage);
+			await _repository.SaveChangesAsync();
+		}
+
+		public async Task CreateUnitAsync(UnitAddFormModel unitModel, bool isUserAdmin)
+		{
+			var unit = new Unit()
+			{
+				Type = unitModel.Type,
+				IsApproved = isUserAdmin ? true : false
+			};
+
+			await _repository.AddAsync<Unit>(unit);
+			await _repository.SaveChangesAsync();
+		}
+
+		public async Task CreateWorkTypeAsync(WorkTypeAddFormModel workTypeModel, bool isUserAdmin)
+		{
+			var workType = new WorkType()
+			{
+				Name = workTypeModel.Name,
+				IsApproved = isUserAdmin ? true : false
+			};
+
+			await _repository.AddAsync<WorkType>(workType);
 			await _repository.SaveChangesAsync();
 		}
 	}
