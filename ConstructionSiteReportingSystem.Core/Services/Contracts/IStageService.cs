abae20d02342b9da54d7660195b4f5e01014c42f
karinanikolova <lianna_kn@yahoo.com>
2024-09-10 17:21:41 +0300

    Added IStageService interface.

---
+++ ConstructionSiteReportingSystem.Core/Services/Contracts/IStageService.cs
@@ -0,0 +1,22 @@
+﻿using ConstructionSiteReportingSystem.Core.Models.Suggest;
+using ConstructionSiteReportingSystem.Core.Models.Work;
+
+namespace ConstructionSiteReportingSystem.Core.Services.Contracts
+{
+	public interface IStageService
+	{
+		Task<StageAddFormModel?> GetStageAddFormModelByIdAsync(int stageId);
+
+		Task<IEnumerable<StageServiceModel>> GetStagesForReviewAsync();
+
+		Task ApproveStageAsync(int stageId);
+
+		Task EditStageAsync(int stageId, StageAddFormModel stageModel);
+
+		Task RemoveStageAsync(int stageId);
+
+		Task<bool> DoesUnapprovedStageExistAsync(int stageId);
+
+		Task<bool> AreThereStagesToApproveAsync();
+	}
+}
