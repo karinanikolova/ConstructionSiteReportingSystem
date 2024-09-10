7e0cb7fc06c827d978ab3dfe97466ddc53d638bb
karinanikolova <lianna_kn@yahoo.com>
2024-09-10 17:21:17 +0300

    Added IContractorService interface.

---
+++ ConstructionSiteReportingSystem.Core/Services/Contracts/IContractorService.cs
@@ -0,0 +1,24 @@
+﻿using ConstructionSiteReportingSystem.Core.Models.Suggest;
+using ConstructionSiteReportingSystem.Core.Models.Work;
+
+namespace ConstructionSiteReportingSystem.Core.Services.Contracts
+{
+	public interface IContractorService
+    {
+        Task<ContractorAddFormModel?> GetContractorAddFormModelByIdAsync(int contractorId);
+
+		Task<IEnumerable<ContractorServiceModel>> GetContractorsForReviewAsync();
+
+		Task<IEnumerable<WorkTypeServiceModel>> GetWorkTypesForReviewAsync();
+
+		Task ApproveContractorAsync(int contractorId);
+
+		Task EditContractorAsync(int contractorId, ContractorAddFormModel contractorModel);
+
+		Task RemoveContractorAsync(int contractorId);
+
+		Task<bool> DoesUnapprovedContractorExistAsync(int contractorId);
+
+		Task<bool> AreThereContractorsToApproveAsync();
+	}
+}
