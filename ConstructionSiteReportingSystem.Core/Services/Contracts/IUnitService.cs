11230a68657e0b258d9cf0dc9dc834515620dd0e
karinanikolova <lianna_kn@yahoo.com>
2024-09-10 17:22:04 +0300

    Added IUnitService interface.

---
+++ ConstructionSiteReportingSystem.Core/Services/Contracts/IUnitService.cs
@@ -0,0 +1,22 @@
+﻿using ConstructionSiteReportingSystem.Core.Models.Suggest;
+using ConstructionSiteReportingSystem.Core.Models.Work;
+
+namespace ConstructionSiteReportingSystem.Core.Services.Contracts
+{
+	public interface IUnitService
+	{
+		Task<UnitAddFormModel?> GetUnitAddFormModelByIdAsync(int unitId);
+
+		Task<IEnumerable<UnitServiceModel>> GetUnitsForReviewAsync();
+
+		Task ApproveUnitAsync(int unitId);
+
+		Task EditUnitAsync(int unitId, UnitAddFormModel unitModel);
+
+		Task RemoveUnitAsync(int unitId);
+
+		Task<bool> DoesUnapprovedUnitExistAsync(int unitId);
+
+		Task<bool> AreThereUnitsToApproveAsync();
+	}
+}
