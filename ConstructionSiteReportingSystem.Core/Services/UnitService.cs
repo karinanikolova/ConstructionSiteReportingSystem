56924994f6fb732dc4d917d215cf795fdd3330e6
karinanikolova <lianna_kn@yahoo.com>
2024-09-10 17:24:50 +0300

    Added UnitService.

---
+++ ConstructionSiteReportingSystem.Core/Services/UnitService.cs
@@ -0,0 +1,92 @@
+﻿using ConstructionSiteReportingSystem.Core.Models.Suggest;
+using ConstructionSiteReportingSystem.Core.Models.Work;
+using ConstructionSiteReportingSystem.Core.Services.Contracts;
+using ConstructionSiteReportingSystem.Infrastructure.Data.Models;
+using ConstructionSiteReportingSystem.Infrastructure.Data.Utilities.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Task = System.Threading.Tasks.Task;
+
+namespace ConstructionSiteReportingSystem.Core.Services
+{
+	public class UnitService : IUnitService
+	{
+		private readonly IRepository _repository;
+
+		public UnitService(IRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<IEnumerable<UnitServiceModel>> GetUnitsForReviewAsync()
+		{
+			return await _repository.AllReadOnly<Unit>()
+				.Where(u => u.IsApproved == false)
+				.Select(u => new UnitServiceModel()
+				{
+					Id = u.Id,
+					Type = u.Type
+				})
+				.ToListAsync();
+		}
+
+		public async Task<UnitAddFormModel?> GetUnitAddFormModelByIdAsync(int unitId)
+		{
+			return await _repository.AllReadOnly<Unit>()
+				.Where(u => u.Id == unitId)
+				.Select(u => new UnitAddFormModel()
+				{
+					Type = u.Type
+				})
+				.FirstOrDefaultAsync();
+		}
+
+		public async Task ApproveUnitAsync(int unitId)
+		{
+			var unitToApprove = await _repository.GetByIdAsync<Unit>(unitId);
+
+			if (unitToApprove != null && unitToApprove.IsApproved == false)
+			{
+				unitToApprove.IsApproved = true;
+
+				await _repository.SaveChangesAsync();
+			}
+		}
+
+		public async Task<bool> AreThereUnitsToApproveAsync()
+		{
+			return await _repository.AllReadOnly<Unit>()
+				.Where(u => u.IsApproved == false)
+				.AnyAsync();
+		}
+
+		public async Task<bool> DoesUnapprovedUnitExistAsync(int unitId)
+		{
+			return await _repository.AllReadOnly<Unit>()
+				.Where(u => u.IsApproved == false)
+				.AnyAsync(u => u.Id == unitId);
+		}
+
+		public async Task EditUnitAsync(int unitId, UnitAddFormModel unitModel)
+		{
+			var unit = await _repository.GetByIdAsync<Unit>(unitId);
+
+			if (unit != null)
+			{
+				unit.Type = unitModel.Type.Trim();
+			}
+
+			await _repository.SaveChangesAsync();
+		}
+
+		public async Task RemoveUnitAsync(int unitId)
+		{
+			var unitToRemove = await _repository.GetByIdAsync<Unit>(unitId);
+
+			if (unitToRemove != null && unitToRemove.IsApproved == false)
+			{
+				_repository.Delete<Unit>(unitToRemove);
+				await _repository.SaveChangesAsync();
+			}
+		}
+	}
+}
