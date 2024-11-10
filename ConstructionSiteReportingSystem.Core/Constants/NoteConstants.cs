dc87d2c238d208d522d3ebc2ba71b2c9aa2859cc
karinanikolova <lianna_kn@yahoo.com>
2024-11-10 19:50:25 +0200

    Added NoteConstants class for holding note messages to use in Suggest views.

---
+++ ConstructionSiteReportingSystem.Core/Constants/NoteConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace ConstructionSiteReportingSystem.Core.Constants
+{
+	public static class NoteConstants
+	{
+		public const string SuggestContractorNote = "Take into consideration that your suggestion will be manually reviewed by the site administrator and will only appear if approved. Combination of letters, digits, space and \"-\" special character is only permitted.";
+
+		public const string SuggestStageNote = "Take into consideration that your suggestion will be manually reviewed by the site administrator and will only appear if approved. Combination of letters, digits, space and \"-\" special character is only permitted.";
+
+		public const string SuggestUnitNote = "Take into consideration that your suggestion will be manually reviewed by the site administrator and will only appear if approved. Combination of letters, digits, space and special characters such as \".\", \"^\", \"/\", \" ' \" is only permitted.";
+
+		public const string SuggestWorkTypeNote = "Take into consideration that your suggestion will be manually reviewed by the site administrator and will only appear if approved. Combination of letters, digits, space and special characters such as \"-\", \".\", \",\", \"(\", \")\" is only permitted.";
+	}
+}
