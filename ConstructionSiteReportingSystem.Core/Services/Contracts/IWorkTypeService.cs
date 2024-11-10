b13c0db057a07a3839eec5c17a3072561068efd4
karinanikolova <lianna_kn@yahoo.com>
2024-11-10 20:04:38 +0200

    Added IWorkTypeService interface.

---
+++ ConstructionSiteReportingSystem.Core/Services/Contracts/IWorkTypeService.cs
@@ -0,0 +1,22 @@
+﻿using ConstructionSiteReportingSystem.Core.Models.Suggest;
+using ConstructionSiteReportingSystem.Core.Models.Work;
+
+namespace ConstructionSiteReportingSystem.Core.Services.Contracts
+{
+	public interface IWorkTypeService
+    {
+		Task<WorkTypeAddFormModel?> GetWorkTypeAddFormModelByIdAsync(int workTypeId);
+
+		Task<IEnumerable<WorkTypeServiceModel>> GetWorkTypesForReviewAsync();
+
+		Task ApproveWorkTypeAsync(int workTypeId);
+
+		Task EditWorkTypeAsync(int workTypeId, WorkTypeAddFormModel workTypeModel);
+
+		Task RemoveWorkTypeAsync(int workTypeId);
+
+		Task<bool> DoesUnapprovedWorkTypeExistAsync(int workTypeId);
+
+		Task<bool> AreThereWorkTypesToApproveAsync();
+	}
+}
