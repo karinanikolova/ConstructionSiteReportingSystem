0866dad35116f20ef1a766b8c9bc9a43e02c59af
karinanikolova <lianna_kn@yahoo.com>
2025-01-14 16:54:35 +0200

    Deleted SiteViewModel.

--- ConstructionSiteReportingSystem.Core/Models/Site/SiteViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConstructionSiteReportingSystem.Core.Models.Site
-{
-    public class SiteViewModel
-    {
-        public string Name { get; set; } = string.Empty;
-
-        public DateTime FinishDate { get; set; }
-    }
-}
