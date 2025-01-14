0abfa72f599fe9a91a6cb1236a0032f091eff64c
karinanikolova <lianna_kn@yahoo.com>
2025-01-14 16:56:21 +0200

    Added SiteAddFormModel in Models/Admin/Site directory.

---
+++ ConstructionSiteReportingSystem.Core/Models/Admin/Site/SiteAddFormModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ConstructionSiteReportingSystem.Infrastructure.Constants.DataConstants.Site;
+using static ConstructionSiteReportingSystem.Core.Constants.MessageConstants;
+
+namespace ConstructionSiteReportingSystem.Core.Models.Admin.Site
+{
+    public class SiteAddFormModel
+    {
+        [Required(ErrorMessage = RequiredFieldMessage)]
+        [StringLength(NameMaxLength, MinimumLength = NameMinLength, ErrorMessage = FieldLengthMessage)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = DateMessage)]
+        [Display(Name = "Finish date")]
+        public string FinishDate { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredFieldMessage)]
+		[Display(Name = "Image URL")]
+        [Url]
+		public string ImageUrl { get; set; } = string.Empty;
+    }
+}
