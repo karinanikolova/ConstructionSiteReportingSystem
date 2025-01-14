3ffcab5ea71364fab718a921b17e3881fb71af13
karinanikolova <lianna_kn@yahoo.com>
2025-01-14 16:55:44 +0200

    Added SiteViewModel in Models/Admin/Site directory.

---
+++ ConstructionSiteReportingSystem.Core/Models/Admin/Site/SiteViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ConstructionSiteReportingSystem.Core.Models.Admin.Site
+{
+	public class SiteViewModel
+    {
+		public int Id { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+
+        public DateTime FinishDate { get; set; }
+
+        public int WorksCount { get; set; }
+
+        public IEnumerable<int> WorkIds { get; set; } = new List<int>();
+
+        public int UsersPostedCount { get; set; }
+    }
+}
