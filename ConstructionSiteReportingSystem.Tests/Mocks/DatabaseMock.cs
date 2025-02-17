d125d325c673dbec38f08330a2966406b29ea6fc
karinanikolova <lianna_kn@yahoo.com>
2025-02-17 20:59:42 +0200

    Created a DatabaseMock static class with an Instance property for creating an in-memory database for unit testing.

---
+++ ConstructionSiteReportingSystem.Tests/Mocks/DatabaseMock.cs
@@ -0,0 +1,23 @@
+﻿using ConstructionSiteReportingSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConstructionSiteReportingSystem.Tests.Mocks
+{
+	/// <summary>
+	/// Static class for database mock which provides Instance property for creating an in-memory database with a unique name without any seeded data.
+	/// </summary>
+	public static class DatabaseMock
+	{
+		public static ConstructionSiteDbContext Instance
+		{
+			get
+			{
+				var options = new DbContextOptionsBuilder<ConstructionSiteDbContext>()
+					.UseInMemoryDatabase("ConstructionSiteInMemoryDb" + DateTime.Now.Ticks.ToString())
+					.Options;
+
+				return new ConstructionSiteDbContext(options, false);
+			}
+		}
+	}
+}
