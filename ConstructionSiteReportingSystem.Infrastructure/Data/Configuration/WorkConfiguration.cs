10a4a53fe9004c720ddba937bac310216c37c757
karinanikolova <lianna_kn@yahoo.com>
2025-02-17 20:49:50 +0200

    Removed query filter for Work inWorkConfiguration.

--- ConstructionSiteReportingSystem.Infrastructure/Data/Configuration/WorkConfiguration.cs
+++ ConstructionSiteReportingSystem.Infrastructure/Data/Configuration/WorkConfiguration.cs
@@ -39,9 +39,6 @@ namespace ConstructionSiteReportingSystem.Infrastructure.Data.Configuration
                 .HasForeignKey(w => w.UnitId)
                 .OnDelete(DeleteBehavior.Restrict);
 
-            // Applying query filer to the entity in order for Entity Framework Core to ignore soft-deleted records when executing queries. The query filer will only show entities with the IsDeleted property set to 'false'.
-            builder.HasQueryFilter(c => c.IsDeleted == false);
-
             // Seeding data for Works
             var data = new DataSeed();
 
